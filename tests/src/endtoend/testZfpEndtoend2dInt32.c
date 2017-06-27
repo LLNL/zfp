9cf5feba7e1c78bd75f9b63f37ad7099fec269b8
Markus Salasoo <salasoo1@llnl.gov>
2017-06-26 17:12:11 -0700

    End to end tests: fixed rate/prec for all remaining {1/2/3d} x {32/64bit} integer combinations

---
+++ tests/src/endtoend/testZfpEndtoend2dInt32.c
@@ -0,0 +1,20 @@
+#include "src/encode2i.c"
+
+#include "constants/2dInt32.h"
+#include "utils/testMacros.h"
+#include "utils/genSmoothRandInts.h"
+#include "utils/hash32.c"
+#include "zfpEndtoendBase.c"
+
+int main()
+{
+  const struct CMUnitTest tests[] = {
+    cmocka_unit_test_setup_teardown(when_seededRandomSmoothDataGenerated_expect_ChecksumMatches, setupFixedPrec, teardown),
+    cmocka_unit_test_setup_teardown(given_2dInt32Array_when_ZfpCompress_expect_BitstreamChecksumMatches, setupFixedPrec, teardown),
+    cmocka_unit_test_setup_teardown(given_2dInt32Array_when_ZfpDecompress_expect_ArrayChecksumMatches, setupFixedPrec, teardown),
+    cmocka_unit_test_setup_teardown(given_2dInt32Array_when_ZfpCompress_expect_BitstreamChecksumMatches, setupFixedRate, teardown),
+    cmocka_unit_test_setup_teardown(given_2dInt32Array_when_ZfpDecompress_expect_ArrayChecksumMatches, setupFixedRate, teardown),
+    cmocka_unit_test_setup_teardown(given_2dInt32FixedRate_when_ZfpCompress_expect_CompressedBitrateComparableToChosenRate, setupFixedRate, teardown),
+  };
+  return cmocka_run_group_tests(tests, NULL, NULL);
+}
---
+++ tests/src/endtoend/testZfpEndtoend3dInt64.c
@@ -0,0 +1,20 @@
+#include "src/encode3l.c"
+
+#include "constants/3dInt64.h"
+#include "utils/testMacros.h"
+#include "utils/genSmoothRandInts.h"
+#include "utils/hash64.c"
+#include "zfpEndtoendBase.c"
+
+int main()
+{
+  const struct CMUnitTest tests[] = {
+    cmocka_unit_test_setup_teardown(when_seededRandomSmoothDataGenerated_expect_ChecksumMatches, setupFixedPrec, teardown),
+    cmocka_unit_test_setup_teardown(given_3dInt64Array_when_ZfpCompress_expect_BitstreamChecksumMatches, setupFixedPrec, teardown),
+    cmocka_unit_test_setup_teardown(given_3dInt64Array_when_ZfpDecompress_expect_ArrayChecksumMatches, setupFixedPrec, teardown),
+    cmocka_unit_test_setup_teardown(given_3dInt64Array_when_ZfpCompress_expect_BitstreamChecksumMatches, setupFixedRate, teardown),
+    cmocka_unit_test_setup_teardown(given_3dInt64Array_when_ZfpDecompress_expect_ArrayChecksumMatches, setupFixedRate, teardown),
+    cmocka_unit_test_setup_teardown(given_3dInt64FixedRate_when_ZfpCompress_expect_CompressedBitrateComparableToChosenRate, setupFixedRate, teardown),
+  };
+  return cmocka_run_group_tests(tests, NULL, NULL);
+}
