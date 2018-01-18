b2514e7c7d70eebfdde17a0883a99278a3f71922
Markus Salasoo <salasoo1@llnl.gov>
2018-01-18 14:09:42 -0800

    versioning: zfpLegacyAdapter provides compatibility between latest and older versions' structs (zfp_stream, zfp_field only)

---
+++ include/versioning/zfpLegacyAdapter.h
@@ -0,0 +1,44 @@
+#ifndef ZFP_LEGACY_ADAPTER_H
+#define ZFP_LEGACY_ADAPTER_H
+
+/* this file is not intended to be included because
+ * it is lacking its own #include statements */
+
+/* Each function converts latest struct to
+ * an older version struct, or vice-versa
+ * Returns 0 if struct data cannot be preserved or is incompatible
+ * Returns 1 on success */
+
+#ifdef ZFP_V4_DIR
+
+/* zfp_stream */
+
+int
+zfp_stream_latest_to_v4(
+  const zfp_stream* stream,
+  zfp_v4_stream* stream_v4
+);
+
+int
+zfp_stream_v4_to_latest(
+  const zfp_v4_stream* stream_v4,
+  zfp_stream* stream
+);
+
+/* zfp_field */
+
+int
+zfp_field_latest_to_v4(
+  const zfp_field* field,
+  zfp_v4_field* field_v4
+);
+
+int
+zfp_field_v4_to_latest(
+  const zfp_v4_field* field_v4,
+  zfp_field* field
+);
+
+#endif
+
+#endif
---
+++ src/versioning/zfpLegacyAdapter.c
@@ -0,0 +1,71 @@
+#include "versioning/zfpLegacyAdapter.h"
+
+#ifdef ZFP_V4_DIR
+
+/* zfp_stream */
+int
+zfp_stream_latest_to_v4(const zfp_stream *zfp, zfp_v4_stream *zfp_v4)
+{
+  uint64 params = zfp_stream_mode(zfp);
+  return zfp_v4_stream_set_mode(zfp_v4, params);
+}
+
+int
+zfp_stream_v4_to_latest(const zfp_v4_stream *zfp_v4, zfp_stream *zfp)
+{
+  uint64 params = zfp_v4_stream_mode(zfp_v4);
+  return zfp_stream_set_mode(zfp, params);
+}
+
+/* zfp_field */
+int
+zfp_field_latest_to_v4(const zfp_field *field, zfp_v4_field *field_v4)
+{
+  void* data_ptr = zfp_field_pointer(field);
+  zfp_v4_field_set_pointer(field_v4, data_ptr);
+
+  uint64 metadata = zfp_field_metadata(field);
+  int result = zfp_v4_field_set_metadata(field_v4, metadata);
+
+  switch(zfp_field_dimensionality(field)) {
+    case 3:
+      field_v4->sz = field->sz;
+    case 2:
+      field_v4->sy = field->sy;
+    case 1:
+      field_v4->sx = field->sx;
+      break;
+
+    default:
+      return 0;
+  }
+
+  return result;
+}
+
+int
+zfp_field_v4_to_latest(const zfp_v4_field *field_v4, zfp_field *field)
+{
+  void* data_ptr = zfp_v4_field_pointer(field_v4);
+  zfp_field_set_pointer(field, data_ptr);
+
+  uint64 metadata = zfp_v4_field_metadata(field_v4);
+  int result = zfp_field_set_metadata(field, metadata);
+
+  switch(zfp_v4_field_dimensionality(field_v4)) {
+    case 3:
+      field->sz = field_v4->sz;
+    case 2:
+      field->sy = field_v4->sy;
+    case 1:
+      field->sx = field_v4->sx;
+      break;
+
+    default:
+      return 0;
+  }
+
+  return result;
+}
+
+#endif
