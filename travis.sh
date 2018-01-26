94e9d305d11e3660649e2f39cbabf704f455b01e
Markus Salasoo <salasoo1@llnl.gov>
2018-01-25 18:11:20 -0800

    versioning: update build scripts (travis, appveyor) to additionally build/test prefixed and combined libs

--- travis.sh
+++ travis.sh
@@ -1,9 +1,22 @@
 #!/usr/bin/env sh
 set -e
 
+# copy/create "older codec" project at ../zfpV4
+./copyScript.sh
+
+# build/test plain lib
 mkdir build
 cd build
 cmake .. -DCMAKE_C_STANDARD=${C_STANDARD:-99} -DCMAKE_CXX_STANDARD=${CXX_STANDARD:-98}
 cmake --build .
 ctest -V -C "Debug"
 
+# build/test prefixed lib
+cmake .. -DCMAKE_C_STANDARD=${C_STANDARD:-99} -DCMAKE_CXX_STANDARD=${CXX_STANDARD:-98} -DZFP_WITH_VERSION_PREFIX=ON
+cmake --build .
+ctest -V -C "Debug"
+
+# build/test combined (2 versions) lib
+cmake .. -DCMAKE_C_STANDARD=${C_STANDARD:-99} -DCMAKE_CXX_STANDARD=${CXX_STANDARD:-98} -DZFP_V4_DIR="../../zfpV4"
+cmake --build .
+ctest -V -C "Debug"
