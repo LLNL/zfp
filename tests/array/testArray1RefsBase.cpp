380ba64485463b3e2a95edf05dafd31fd9826f17
Markus Salasoo <salasoo1@llnl.gov>
2018-03-18 00:56:16 -0700

    test compressed arrays: deep copy, const accessors

--- tests/array/testArray1RefsBase.cpp
+++ tests/array/testArray1RefsBase.cpp
@@ -11,7 +11,27 @@ TEST_F(ARRAY_DIMS_SCALAR_TEST_REFS, when_resize_then_sizeChanges)
 TEST_F(ARRAY_DIMS_SCALAR_TEST_REFS, when_getIndexWithParentheses_then_refReturned)
 {
   uint i = 1;
-  arr[i] = VAL;
+  arr(i) = VAL;
 
   EXPECT_EQ(VAL, arr(i));
+}
+
+TEST_F(ARRAY_DIMS_SCALAR_TEST_REFS, given_constCompressedArray_when_getIndexWithBrackets_then_valReturned)
+{
+  uint i = 1;
+  arr[i] = VAL;
+
+  const array1<SCALAR> arrConst = arr;
+
+  EXPECT_EQ(VAL, arrConst[i]);
+}
+
+TEST_F(ARRAY_DIMS_SCALAR_TEST_REFS, given_constCompressedArray_when_getIndexWithParentheses_then_valReturned)
+{
+  uint i = 1;
+  arr[i] = VAL;
+
+  const array1<SCALAR> arrConst = arr;
+
+  EXPECT_EQ(VAL, arrConst(i));
 }
