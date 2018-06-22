5f43f773f7844b6f7e21524bc9d20378378a983e
mclarsen <larsen30@llnl.gov>
2018-06-22 14:14:36 -0700

    allowing compilation without cuda

--- src/template/cudacompress.c
+++ src/template/cudacompress.c
@@ -3,27 +3,35 @@
 static void
 _t2(compress_cuda, Scalar, 1)(zfp_stream* stream, const zfp_field* field)
 {
+#ifdef ZFP_ENABLE_CUDA
   cuda_compress(stream, field);   
+#endif
 }
 
 /* compress 1d strided array */
 static void
 _t2(compress_strided_cuda, Scalar, 1)(zfp_stream* stream, const zfp_field* field)
 {
+#ifdef ZFP_ENABLE_CUDA
   cuda_compress(stream, field);   
+#endif
 }
 
 /* compress 2d strided array */
 static void
 _t2(compress_strided_cuda, Scalar, 2)(zfp_stream* stream, const zfp_field* field)
 {
+#ifdef ZFP_ENABLE_CUDA
   cuda_compress(stream, field);   
+#endif
 }
 
 /* compress 3d strided array */
 static void
 _t2(compress_strided_cuda, Scalar, 3)(zfp_stream* stream, const zfp_field* field)
 {
+#ifdef ZFP_ENABLE_CUDA
   cuda_compress(stream, field);   
+#endif
 }
 
--- src/template/cudadecompress.c
+++ src/template/cudadecompress.c
@@ -3,27 +3,35 @@
 static void
 _t2(decompress_cuda, Scalar, 1)(zfp_stream* stream, zfp_field* field)
 {
+#ifdef ZFP_ENABLE_CUDA
   cuda_decompress(stream, field);   
+#endif
 }
 
 /* compress 1d strided array */
 static void
 _t2(decompress_strided_cuda, Scalar, 1)(zfp_stream* stream, zfp_field* field)
 {
+#ifdef ZFP_ENABLE_CUDA
   cuda_decompress(stream, field);   
+#endif
 }
 
 /* compress 2d strided array */
 static void
 _t2(decompress_strided_cuda, Scalar, 2)(zfp_stream* stream, zfp_field* field)
 {
+#ifdef ZFP_ENABLE_CUDA
   cuda_decompress(stream, field);   
+#endif
 }
 
 /* compress 3d strided array */
 static void
 _t2(decompress_strided_cuda, Scalar, 3)(zfp_stream* stream, zfp_field* field)
 {
+#ifdef ZFP_ENABLE_CUDA
   cuda_decompress(stream, field);   
+#endif
 }
 
