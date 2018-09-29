20658c098312c0c81de5a202415cd8bbe48a7dd9
Markus Salasoo <salasoo1@llnl.gov>
2018-09-29 12:59:08 -0700

    Test constructing compressed arrays from 2D views

--- tests/array/testArray2f.cpp
+++ tests/array/testArray2f.cpp
@@ -26,6 +26,7 @@ class Array2fTest : public ArrayNdTestFixture {};
 #define SCALAR float
 #define DIMS 2
 #include "testArrayBase.cpp"
+#include "testArray2Base.cpp"
 
 int main(int argc, char* argv[]) {
   ::testing::InitGoogleTest(&argc, argv);
