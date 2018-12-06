7206eefefa055542b3a8c7daf5c52153c5c33944
Markus Salasoo <salasoo1@llnl.gov>
2018-12-05 17:35:11 -0800

    Add fortran bindings for zfp_field API functions, and compression/decompression

--- fortran/zfp.f
+++ fortran/zfp.f
@@ -1,6 +1,6 @@
 module zFORp_module
 
-  use, intrinsic :: iso_c_binding, only: c_int, c_int64_t, c_size_t, c_double, c_ptr, c_null_ptr
+  use, intrinsic :: iso_c_binding, only: c_int, c_int64_t, c_size_t, c_double, c_ptr, c_null_ptr, c_loc
   implicit none
   private
 
@@ -13,6 +13,11 @@ module zFORp_module
     private
     type(c_ptr) :: object = c_null_ptr
   end type zFORp_stream_type
+
+  type zFORp_field_type
+    private
+    type(c_ptr) :: object = c_null_ptr
+  end type zFORp_field_type
 
   enum, bind(c)
     enumerator :: zFORp_type_none = 0, &
@@ -133,12 +138,189 @@ module zFORp_module
       integer(c_int) is_success
     end function
 
+    ! high-level API: zfp_field functions
+
+    function zfp_field_alloc() result(zfp_field) bind(c, name="zfp_field_alloc")
+      import
+      type(c_ptr) :: zfp_field
+    end function
+
+    function zfp_field_1d(uncompressed_ptr, zfp_type, nx) result(zfp_field) bind(c, name="zfp_field_1d")
+      import
+      type(c_ptr), value :: uncompressed_ptr
+      type(c_ptr) :: zfp_field
+      integer(c_int), value :: zfp_type, nx
+    end function
+
+    function zfp_field_2d(uncompressed_ptr, zfp_type, nx, ny) result(zfp_field) bind(c, name="zfp_field_2d")
+      import
+      type(c_ptr), value :: uncompressed_ptr
+      type(c_ptr) :: zfp_field
+      integer(c_int), value :: zfp_type, nx, ny
+    end function
+
+    function zfp_field_3d(uncompressed_ptr, zfp_type, nx, ny, nz) result(zfp_field) bind(c, name="zfp_field_3d")
+      import
+      type(c_ptr), value :: uncompressed_ptr
+      type(c_ptr) :: zfp_field
+      integer(c_int), value :: zfp_type, nx, ny, nz
+    end function
+
+    function zfp_field_4d(uncompressed_ptr, zfp_type, nx, ny, nz, nw) result(zfp_field) bind(c, name="zfp_field_4d")
+      import
+      type(c_ptr), value :: uncompressed_ptr
+      type(c_ptr) :: zfp_field
+      integer(c_int), value :: zfp_type, nx, ny, nz, nw
+    end function
+
+    subroutine zfp_field_free(zfp_field) bind(c, name="zfp_field_free")
+      import
+      type(c_ptr), value :: zfp_field
+    end subroutine
+
+    function zfp_field_pointer(zfp_field) result(arr_ptr) bind(c, name="zfp_field_pointer")
+      import
+      type(c_ptr), value :: zfp_field
+      type(c_ptr) :: arr_ptr
+    end function
+
+    function zfp_field_scalar_type(zfp_field) result(zfp_type) bind(c, name="zfp_field_type")
+      import
+      type(c_ptr), value :: zfp_field
+      integer(c_int) zfp_type
+    end function
+
+    function zfp_field_precision(zfp_field) result(prec) bind(c, name="zfp_field_precision")
+      import
+      type(c_ptr), value :: zfp_field
+      integer(c_int) prec
+    end function
+
+    function zfp_field_dimensionality(zfp_field) result(dims) bind(c, name="zfp_field_dimensionality")
+      import
+      type(c_ptr), value :: zfp_field
+      integer(c_int) dims
+    end function
+
+    function zfp_field_size(zfp_field, size_arr) result(total_size) bind(c, name="zfp_field_size")
+      import
+      type(c_ptr), value :: zfp_field, size_arr
+      integer(c_size_t) total_size
+    end function
+
+    function zfp_field_stride(zfp_field, stride_arr) result(is_strided) bind(c, name="zfp_field_stride")
+      import
+      type(c_ptr), value :: zfp_field, stride_arr
+      integer(c_int) is_strided
+    end function
+
+    function zfp_field_metadata(zfp_field) result(encoded_metadata) bind(c, name="zfp_field_metadata")
+      import
+      type(c_ptr), value :: zfp_field
+      integer(c_int64_t) encoded_metadata
+    end function
+
+    subroutine zfp_field_set_pointer(zfp_field, arr_ptr) bind(c, name="zfp_field_set_pointer")
+      import
+      type(c_ptr), value :: zfp_field, arr_ptr
+    end subroutine
+
+    function zfp_field_set_type(zfp_field, zfp_type) result(zfp_type_result) bind(c, name="zfp_field_set_type")
+      import
+      type(c_ptr), value :: zfp_field
+      integer(c_int) zfp_type, zfp_type_result
+    end function
+
+    subroutine zfp_field_set_size_1d(zfp_field, nx) bind(c, name="zfp_field_set_size_1d")
+      import
+      type(c_ptr), value :: zfp_field
+      integer(c_int) nx
+    end subroutine
+
+    subroutine zfp_field_set_size_2d(zfp_field, nx, ny) bind(c, name="zfp_field_set_size_2d")
+      import
+      type(c_ptr), value :: zfp_field
+      integer(c_int) nx, ny
+    end subroutine
+
+    subroutine zfp_field_set_size_3d(zfp_field, nx, ny, nz) bind(c, name="zfp_field_set_size_3d")
+      import
+      type(c_ptr), value :: zfp_field
+      integer(c_int) nx, ny, nz
+    end subroutine
+
+    subroutine zfp_field_set_size_4d(zfp_field, nx, ny, nz, nw) bind(c, name="zfp_field_set_size_4d")
+      import
+      type(c_ptr), value :: zfp_field
+      integer(c_int) nx, ny, nz, nw
+    end subroutine
+
+    subroutine zfp_field_set_stride_1d(zfp_field, sx) bind(c, name="zfp_field_set_stride_1d")
+      import
+      type(c_ptr), value :: zfp_field
+      integer(c_int) sx
+    end subroutine
+
+    subroutine zfp_field_set_stride_2d(zfp_field, sx, sy) bind(c, name="zfp_field_set_stride_2d")
+      import
+      type(c_ptr), value :: zfp_field
+      integer(c_int) sx, sy
+    end subroutine
+
+    subroutine zfp_field_set_stride_3d(zfp_field, sx, sy, sz) bind(c, name="zfp_field_set_stride_3d")
+      import
+      type(c_ptr), value :: zfp_field
+      integer(c_int) sx, sy, sz
+    end subroutine
+
+    subroutine zfp_field_set_stride_4d(zfp_field, sx, sy, sz, sw) bind(c, name="zfp_field_set_stride_4d")
+      import
+      type(c_ptr), value :: zfp_field
+      integer(c_int) sx, sy, sz, sw
+    end subroutine
+
+    function zfp_field_set_metadata(zfp_field, encoded_metadata) result(is_success) bind(c, name="zfp_field_set_metadata")
+      import
+      type(c_ptr), value :: zfp_field
+      integer(c_int64_t) :: encoded_metadata
+      integer(c_int) is_success
+    end function
+
+    ! high-level API: compression and decompression
+
+    function zfp_compress(zfp_stream, zfp_field) result(bitstream_offset_bytes) bind(c, name="zfp_compress")
+      import
+      type(c_ptr), value :: zfp_stream, zfp_field
+      integer(c_size_t) :: bitstream_offset_bytes
+    end function
+
+    function zfp_decompress(zfp_stream, zfp_field) result(bitstream_offset_bytes) bind(c, name="zfp_decompress")
+      import
+      type(c_ptr), value :: zfp_stream, zfp_field
+      integer(c_size_t) :: bitstream_offset_bytes
+    end function
+
+    function zfp_write_header(zfp_stream, zfp_field, mask) result(num_bits_written) bind(c, name="zfp_write_header")
+      import
+      type(c_ptr), value :: zfp_stream, zfp_field
+      integer(c_int) mask
+      integer(c_size_t) num_bits_written
+    end function
+
+    function zfp_read_header(zfp_stream, zfp_field, mask) result(num_bits_read) bind(c, name="zfp_read_header")
+      import
+      type(c_ptr), value :: zfp_stream, zfp_field
+      integer(c_int) mask
+      integer(c_size_t) num_bits_read
+    end function
+
   end interface
 
   ! types
 
   public :: zFORp_bitstream_type, &
-            zFORp_stream_type
+            zFORp_stream_type, &
+            zFORp_field_type
 
   ! enums
 
@@ -174,6 +356,40 @@ module zFORp_module
             zFORp_stream_set_accuracy, &
             zFORp_stream_set_mode, &
             zFORp_stream_set_params
+
+  ! high-level API: zfp_field functions
+
+  public :: zFORp_field_alloc, &
+            zFORp_field_1d, &
+            zFORp_field_2d, &
+            zFORp_field_3d, &
+            zFORp_field_4d, &
+            zFORp_field_free, &
+            zFORp_field_pointer, &
+            zFORp_field_scalar_type, &
+            zFORp_field_precision, &
+            zFORp_field_dimensionality, &
+            zFORp_field_size, &
+            zFORp_field_stride, &
+            zFORp_field_metadata, &
+            zFORp_field_set_pointer, &
+            zFORp_field_set_type, &
+            zFORp_field_set_size_1d, &
+            zFORp_field_set_size_2d, &
+            zFORp_field_set_size_3d, &
+            zFORp_field_set_size_4d, &
+            zFORp_field_set_stride_1d, &
+            zFORp_field_set_stride_2d, &
+            zFORp_field_set_stride_3d, &
+            zFORp_field_set_stride_4d, &
+            zFORp_field_set_metadata
+
+  ! high-level API: compression and decompression
+
+  public :: zFORp_compress, &
+            zFORp_decompress, &
+            zFORp_write_header, &
+            zFORp_read_header
 
 contains
 
@@ -298,5 +514,215 @@ contains
                                        int(maxprec, c_int), &
                                        int(minexp, c_int))
   end function zFORp_stream_set_params
+
+  ! high-level API: zfp_field functions
+
+  function zFORp_field_alloc() result(zfp_field)
+    implicit none
+    type(zFORp_field_type) zfp_field
+    zfp_field%object = zfp_field_alloc()
+  end function zFORp_field_alloc
+
+  function zFORp_field_1d(uncompressed_ptr, zfp_type, nx) result(zfp_field)
+    implicit none
+    type(c_ptr), intent(in) :: uncompressed_ptr
+    integer, intent(in) :: zfp_type, nx
+    type(zFORp_field_type) zfp_field
+    zfp_field%object = zfp_field_1d(uncompressed_ptr, int(zfp_type, c_int), &
+                                    int(nx, c_int))
+  end function zFORp_field_1d
+
+  function zFORp_field_2d(uncompressed_ptr, zfp_type, nx, ny) result(zfp_field)
+    implicit none
+    type(c_ptr), intent(in) :: uncompressed_ptr
+    integer, intent(in) :: zfp_type, nx, ny
+    type(zFORp_field_type) zfp_field
+    zfp_field%object = zfp_field_2d(uncompressed_ptr, int(zfp_type, c_int), &
+                                    int(nx, c_int), int(ny, c_int))
+  end function zFORp_field_2d
+
+  function zFORp_field_3d(uncompressed_ptr, zfp_type, nx, ny, nz) result(zfp_field)
+    implicit none
+    type(c_ptr), intent(in) :: uncompressed_ptr
+    integer, intent(in) :: zfp_type, nx, ny, nz
+    type(zFORp_field_type) zfp_field
+    zfp_field%object = zfp_field_3d(uncompressed_ptr, int(zfp_type, c_int), &
+                                    int(nx, c_int), int(ny, c_int), &
+                                    int(nz, c_int))
+  end function zFORp_field_3d
+
+  function zFORp_field_4d(uncompressed_ptr, zfp_type, nx, ny, nz, nw) result(zfp_field)
+    implicit none
+    type(c_ptr), intent(in) :: uncompressed_ptr
+    integer, intent(in) :: zfp_type, nx, ny, nz, nw
+    type(zFORp_field_type) zfp_field
+    zfp_field%object = zfp_field_4d(uncompressed_ptr, int(zfp_type, c_int), &
+                                    int(nx, c_int), int(ny, c_int), &
+                                    int(nz, c_int), int(nw, c_int))
+  end function zFORp_field_4d
+
+  subroutine zFORp_field_free(zfp_field)
+    type(zFORp_field_type), intent(inout) :: zfp_field
+    call zfp_field_free(zfp_field%object)
+    zfp_field%object = c_null_ptr
+  end subroutine zFORp_field_free
+
+  function zFORp_field_pointer(zfp_field) result(arr_ptr)
+    implicit none
+    type(zFORp_field_type), intent(in) :: zfp_field
+    type(c_ptr) arr_ptr
+    arr_ptr = zfp_field_pointer(zfp_field%object)
+  end function zFORp_field_pointer
+
+  ! added "scalar" to name to avoid clash with zfp_field_type
+  function zFORp_field_scalar_type(zfp_field) result(zfp_type)
+    implicit none
+    type(zFORp_field_type), intent(in) :: zfp_field
+    integer zfp_type
+    zfp_type = zfp_field_scalar_type(zfp_field%object)
+  end function zFORp_field_scalar_type
+
+  function zFORp_field_precision(zfp_field) result(prec)
+    implicit none
+    type(zFORp_field_type), intent(in) :: zfp_field
+    integer prec
+    prec = zfp_field_precision(zfp_field%object)
+  end function zFORp_field_precision
+
+  function zFORp_field_dimensionality(zfp_field) result(dims)
+    implicit none
+    type(zFORp_field_type), intent(in) :: zfp_field
+    integer dims
+    dims = zfp_field_dimensionality(zfp_field%object)
+  end function zFORp_field_dimensionality
+
+  function zFORp_field_size(zfp_field, size_arr) result(total_size)
+    implicit none
+    type(zFORp_field_type), intent(in) :: zfp_field
+    integer, dimension(:), target, intent(inout) :: size_arr
+    integer total_size
+    total_size = zfp_field_size(zfp_field%object, c_loc(size_arr))
+  end function zFORp_field_size
+
+  function zFORp_field_stride(zfp_field, stride_arr) result(is_strided)
+    implicit none
+    type(zFORp_field_type), intent(in) :: zfp_field
+    integer, dimension(:), target, intent(inout) :: stride_arr
+    integer is_strided
+    is_strided = zfp_field_stride(zfp_field%object, c_loc(stride_arr))
+  end function zFORp_field_stride
+
+  function zFORp_field_metadata(zfp_field) result(encoded_metadata)
+    implicit none
+    type(zFORp_field_type), intent(in) :: zfp_field
+    integer (kind=8) encoded_metadata
+    encoded_metadata = zfp_field_metadata(zfp_field%object)
+  end function zFORp_field_metadata
+
+  subroutine zFORp_field_set_pointer(zfp_field, arr_ptr)
+    type(zFORp_field_type), intent(in) :: zfp_field
+    type(c_ptr), intent(in) :: arr_ptr
+    call zfp_field_set_pointer(zfp_field%object, arr_ptr)
+  end subroutine zFORp_field_set_pointer
+
+  function zFORp_field_set_type(zfp_field, zfp_type) result(zfp_type_result)
+    implicit none
+    type(zFORp_field_type), intent(in) :: zfp_field
+    integer, intent(in) :: zfp_type
+    integer zfp_type_result
+    zfp_type_result = zfp_field_set_type(zfp_field%object, int(zfp_type, c_int))
+  end function zFORp_field_set_type
+
+  subroutine zFORp_field_set_size_1d(zfp_field, nx)
+    type(zFORp_field_type), intent(in) :: zfp_field
+    integer, intent(in) :: nx
+    call zfp_field_set_size_1d(zfp_field%object, int(nx, c_int))
+  end subroutine zFORp_field_set_size_1d
+
+  subroutine zFORp_field_set_size_2d(zfp_field, nx, ny)
+    type(zFORp_field_type), intent(in) :: zfp_field
+    integer, intent(in) :: nx, ny
+    call zfp_field_set_size_2d(zfp_field%object, int(nx, c_int), int(ny, c_int))
+  end subroutine zFORp_field_set_size_2d
+
+  subroutine zFORp_field_set_size_3d(zfp_field, nx, ny, nz)
+    type(zFORp_field_type), intent(in) :: zfp_field
+    integer, intent(in) :: nx, ny, nz
+    call zfp_field_set_size_3d(zfp_field%object, int(nx, c_int), int(ny, c_int), int(nz, c_int))
+  end subroutine zFORp_field_set_size_3d
+
+  subroutine zFORp_field_set_size_4d(zfp_field, nx, ny, nz, nw)
+    type(zFORp_field_type), intent(in) :: zfp_field
+    integer, intent(in) :: nx, ny, nz, nw
+    call zfp_field_set_size_4d(zfp_field%object, int(nx, c_int), int(ny, c_int), int(nz, c_int), int(nw, c_int))
+  end subroutine zFORp_field_set_size_4d
+
+  subroutine zFORp_field_set_stride_1d(zfp_field, sx)
+    type(zFORp_field_type), intent(in) :: zfp_field
+    integer, intent(in) :: sx
+    call zfp_field_set_stride_1d(zfp_field%object, int(sx, c_int))
+  end subroutine zFORp_field_set_stride_1d
+
+  subroutine zFORp_field_set_stride_2d(zfp_field, sx, sy)
+    type(zFORp_field_type), intent(in) :: zfp_field
+    integer, intent(in) :: sx, sy
+    call zfp_field_set_stride_2d(zfp_field%object, int(sx, c_int), int(sy, c_int))
+  end subroutine zFORp_field_set_stride_2d
+
+  subroutine zFORp_field_set_stride_3d(zfp_field, sx, sy, sz)
+    type(zFORp_field_type), intent(in) :: zfp_field
+    integer, intent(in) :: sx, sy, sz
+    call zfp_field_set_stride_3d(zfp_field%object, int(sx, c_int), int(sy, c_int), int(sz, c_int))
+  end subroutine zFORp_field_set_stride_3d
+
+  subroutine zFORp_field_set_stride_4d(zfp_field, sx, sy, sz, sw)
+    type(zFORp_field_type), intent(in) :: zfp_field
+    integer, intent(in) :: sx, sy, sz, sw
+    call zfp_field_set_stride_4d(zfp_field%object, int(sx, c_int), int(sy, c_int), int(sz, c_int), int(sw, c_int))
+  end subroutine zFORp_field_set_stride_4d
+
+  function zFORp_field_set_metadata(zfp_field, encoded_metadata) result(is_success)
+    implicit none
+    type(zFORp_field_type), intent(in) :: zfp_field
+    integer (kind=8), intent(in) :: encoded_metadata
+    integer is_success
+    is_success = zfp_field_set_metadata(zfp_field%object, int(encoded_metadata, c_int64_t))
+  end function zFORp_field_set_metadata
+
+  ! high-level API: compression and decompression
+
+  function zFORp_compress(zfp_stream, zfp_field) result(bitstream_offset_bytes)
+    implicit none
+    type(zFORp_stream_type), intent(in) :: zfp_stream
+    type(zFORp_field_type), intent(in) :: zfp_field
+    integer bitstream_offset_bytes
+    bitstream_offset_bytes = zfp_compress(zfp_stream%object, zfp_field%object)
+  end function zFORp_compress
+
+  function zFORp_decompress(zfp_stream, zfp_field) result(bitstream_offset_bytes)
+    implicit none
+    type(zFORp_stream_type), intent(in) :: zfp_stream
+    type(zFORp_field_type), intent(in) :: zfp_field
+    integer bitstream_offset_bytes
+    bitstream_offset_bytes = zfp_decompress(zfp_stream%object, zfp_field%object)
+  end function zFORp_decompress
+
+  function zFORp_write_header(zfp_stream, zfp_field, mask) result(num_bits_written)
+    implicit none
+    type(zFORp_stream_type), intent(in) :: zfp_stream
+    type(zFORp_field_type), intent(in) :: zfp_field
+    integer, intent(in) :: mask
+    integer num_bits_written
+    num_bits_written = zfp_write_header(zfp_stream%object, zfp_field%object, int(mask, c_int))
+  end function zFORp_write_header
+
+  function zFORp_read_header(zfp_stream, zfp_field, mask) result(num_bits_read)
+    implicit none
+    type(zFORp_stream_type), intent(in) :: zfp_stream
+    type(zFORp_field_type), intent(in) :: zfp_field
+    integer, intent(in) :: mask
+    integer num_bits_read
+    num_bits_read = zfp_read_header(zfp_stream%object, zfp_field%object, int(mask, c_int))
+  end function zFORp_read_header
 
 end module zFORp_module
