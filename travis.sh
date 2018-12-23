fb1fa61209364c665b5b63735c4e5de6b6e9de73
Stephen Herbein <stephen272@gmail.com>
2018-12-23 10:42:29 -0500

    travis: add BUILD_PYTHON to build matrix

--- travis.sh
+++ travis.sh
@@ -4,9 +4,11 @@ set -e
 mkdir build
 cd build
 
+CTEST_FLAGS="-V -C \"Debug\" -DC_STANDARD=${C_STANDARD:-99} -DCXX_STANDARD=${CXX_STANDARD:-98} -DBUILD_PYTHON=${BUILD_PYTHON:-off}"
+
 if [ -n "${COVERAGE}" ]; then
   # build (linux)
-  ctest -V -C "Debug" -DC_STANDARD=${C_STANDARD:-99} -DCXX_STANDARD=${CXX_STANDARD:-98} -DBUILD_CFP=ON -DBUILD_ZFORP=ON -DBUILD_OPENMP=ON -DBUILD_CUDA=OFF -DWITH_COVERAGE=ON -S $TRAVIS_BUILD_DIR/cmake/travis.cmake
+  ctest ${CTEST_FLAGS} -DBUILD_CFP=ON -DBUILD_ZFORP=ON -DBUILD_OPENMP=ON -DBUILD_CUDA=OFF -DWITH_COVERAGE=ON -S $TRAVIS_BUILD_DIR/cmake/travis.cmake
 else
   # build/test without OpenMP, with CFP (and custom namespace), with Fortran (linux only)
   if [[ "$OSTYPE" == "darwin"* ]]; then
@@ -15,7 +17,7 @@ else
     BUILD_ZFORP=ON
   fi
 
-  ctest -V -C "Debug" -DC_STANDARD=${C_STANDARD:-99}  -DCXX_STANDARD=${CXX_STANDARD:-98} -DBUILD_CFP=ON -DCFP_NAMESPACE=cfp2 -DZFP_WITH_ALIGNED_ALLOC=1 -DBUILD_ZFORP=${BUILD_ZFORP} -DBUILD_OPENMP=OFF -DBUILD_CUDA=OFF -S $TRAVIS_BUILD_DIR/cmake/travis.cmake
+  ctest ${CTEST_FLAGS} -DBUILD_CFP=ON -DCFP_NAMESPACE=cfp2 -DBUILD_ZFORP=${BUILD_ZFORP} -DZFP_WITH_ALIGNED_ALLOC=1 -DBUILD_OPENMP=OFF -DBUILD_CUDA=OFF -S $TRAVIS_BUILD_DIR/cmake/travis.cmake
 
   rm -rf ./* ;
 
@@ -24,6 +26,6 @@ else
     rm -rf ./* ;
 
     # build/test with OpenMP
-    ctest -V -C "Debug" -DC_STANDARD=${C_STANDARD:-99} -DCXX_STANDARD=${CXX_STANDARD:-98} -DBUILD_OPENMP=ON -S $TRAVIS_BUILD_DIR/cmake/travis.cmake
+    ctest ${CTEST_FLAGS} -DBUILD_OPENMP=ON -S $TRAVIS_BUILD_DIR/cmake/travis.cmake
   fi
 fi
