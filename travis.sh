ae82c7eb36ca5d4672e446ed608a16a11d729422
Markus Salasoo <salasoo1@llnl.gov>
2019-02-15 17:45:53 -0800

    travis: only attempt openmp build if it's available

--- travis.sh
+++ travis.sh
@@ -14,6 +14,11 @@ else
 
   rm -rf ./* ;
 
-  # build/test with OpenMP
-  ctest -V -C "Debug" -DC_STANDARD=${C_STANDARD:-99} -DCXX_STANDARD=${CXX_STANDARD:-98} -DBUILD_OPENMP=ON -S $TRAVIS_BUILD_DIR/cmake/travis.cmake
+  # if OpenMP available, start a 2nd build with it
+  if cmake ../tests/ci-utils/ ; then
+    rm -rf ./* ;
+
+    # build/test with OpenMP
+    ctest -V -C "Debug" -DC_STANDARD=${C_STANDARD:-99} -DCXX_STANDARD=${CXX_STANDARD:-98} -DBUILD_OPENMP=ON -S $TRAVIS_BUILD_DIR/cmake/travis.cmake
+  fi
 fi
