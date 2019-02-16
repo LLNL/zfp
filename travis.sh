9fcc11b1ac8edec4a935965218ae62c61eadd4f1
Markus Salasoo <salasoo1@llnl.gov>
2019-02-15 17:44:28 -0800

    Consolidate CI builds to {openmp off, cfp on} and {openmp on}

--- travis.sh
+++ travis.sh
@@ -9,11 +9,11 @@ if [ -n "${COVERAGE}" ]; then
   # build
   ctest -V -C "Debug" -DC_STANDARD=${C_STANDARD:-99} -DCXX_STANDARD=${CXX_STANDARD:-98} -DBUILD_CFP=ON -DBUILD_OPENMP=ON -DBUILD_CUDA=OFF -DWITH_COVERAGE=ON -S $TRAVIS_BUILD_DIR/cmake/travis.cmake
 else
-  # build/test without OpenMP, with CFP
-  ctest -V -C "Debug" -DC_STANDARD=${C_STANDARD:-99}  -DCXX_STANDARD=${CXX_STANDARD:-98} -DBUILD_CFP=ON -DBUILD_OPENMP=OFF -DBUILD_CUDA=OFF -S $TRAVIS_BUILD_DIR/cmake/travis.cmake
+  # build/test without OpenMP, with CFP (and custom namespace)
+  ctest -V -C "Debug" -DC_STANDARD=${C_STANDARD:-99}  -DCXX_STANDARD=${CXX_STANDARD:-98} -DBUILD_CFP=ON -DCFP_NAMESPACE=cfp2 -DBUILD_OPENMP=OFF -DBUILD_CUDA=OFF -S $TRAVIS_BUILD_DIR/cmake/travis.cmake
 
   rm -rf ./* ;
 
-  # build/test with OpenMP, with CFP custom namespace
-  ctest -V -C "Debug" -DC_STANDARD=${C_STANDARD:-99} -DCXX_STANDARD=${CXX_STANDARD:-98} -DBUILD_CFP=ON -DCFP_NAMESPACE=cfp2 -DBUILD_OPENMP=ON -S $TRAVIS_BUILD_DIR/cmake/travis.cmake
+  # build/test with OpenMP
+  ctest -V -C "Debug" -DC_STANDARD=${C_STANDARD:-99} -DCXX_STANDARD=${CXX_STANDARD:-98} -DBUILD_OPENMP=ON -S $TRAVIS_BUILD_DIR/cmake/travis.cmake
 fi
