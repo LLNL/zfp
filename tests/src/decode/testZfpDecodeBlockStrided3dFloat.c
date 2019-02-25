fedfca6cb425e959fcfdc9ae7e726e07ca285385
Markus Salasoo <salasoo1@llnl.gov>
2019-02-25 03:09:58 -0800

    extract common decode/ testcases into #included files in testcases/

--- tests/src/decode/testZfpDecodeBlockStrided3dFloat.c
+++ tests/src/decode/testZfpDecodeBlockStrided3dFloat.c
@@ -7,14 +7,7 @@
 int main()
 {
   const struct CMUnitTest tests[] = {
-    cmocka_unit_test_setup_teardown(when_seededRandomDataGenerated_expect_ChecksumMatches, setup, teardown),
-    cmocka_unit_test_setup_teardown(given_3dFloatBlock_when_DecodeBlockStrided_expect_ReturnValReflectsNumBitsReadFromBitstream, setup, teardown),
-    cmocka_unit_test_setup_teardown(given_3dFloatBlock_when_DecodeBlockStrided_expect_OnlyStridedEntriesChangedInDestinationArray, setup, teardown),
-    cmocka_unit_test_setup_teardown(given_3dFloatBlock_when_DecodeBlockStrided_expect_ArrayChecksumMatches, setup, teardown),
-    cmocka_unit_test_setup_teardown(given_3dFloatBlock_when_DecodePartialBlockStrided_expect_ReturnValReflectsNumBitsReadFromBitstream, setup, teardown),
-    cmocka_unit_test_setup_teardown(given_3dFloatBlock_when_DecodePartialBlockStrided_expect_NonStridedEntriesUnchangedInDestinationArray, setup, teardown),
-    cmocka_unit_test_setup_teardown(given_3dFloatBlock_when_DecodePartialBlockStrided_expect_EntriesOutsidePartialBlockBoundsUnchangedInDestinationArray, setup, teardown),
-    cmocka_unit_test_setup_teardown(given_3dFloatBlock_when_DecodePartialBlockStrided_expect_ArrayChecksumMatches, setup, teardown),
+    #include "testcases/blockStrided.c"
   };
   return cmocka_run_group_tests(tests, NULL, NULL);
 }
