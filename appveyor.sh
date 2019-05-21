844ad21aebd602625910f20ebf4d08aeea255345
Markus Salasoo <salasoo1@llnl.gov>
2019-05-21 15:09:30 -0700

    enable cdash helper code in cmake/appveyor.cmake (remove redundant flags that were being set in appveyor.sh)

--- appveyor.sh
+++ appveyor.sh
@@ -3,16 +3,8 @@ set -ex
 
 # pass additional args in $1 (starting with whitespace character)
 run_all () {
-#  run_all_cmd="ctest -V -C $BUILD_TYPE -DGENERATOR=\"$GENERATOR\" -S \"$APPVEYOR_BUILD_FOLDER/cmake/appveyor.cmake\""
-
-  run_all_cmd="cmake -G \"$GENERATOR\" -DCMAKE_BUILD_TYPE=\"$BUILD_TYPE\" .."
+  run_all_cmd="ctest -V -C $BUILD_TYPE -DGENERATOR=\"$GENERATOR\" -S \"$APPVEYOR_BUILD_FOLDER/cmake/appveyor.cmake\""
   eval "${run_all_cmd}$1"
-
-  build_all_cmd="cmake --build . --config $BUILD_TYPE"
-  eval "${build_all_cmd}"
-
-  test_cmd="ctest -V -C $BUILD_TYPE -R test_numpy"
-  eval "${test_cmd}"
 }
 
 # create build dir for out-of-source build
@@ -22,9 +14,9 @@ cd build
 # technically, flags are passed on to cmake/* and actually set there
 # config without OpenMP, with CFP (and custom namespace), with aligned allocations (compressed arrays)
 BUILD_FLAGS=""
-#BUILD_FLAGS="$BUILD_FLAGS -DBUILD_UTILITIES=ON"
-#BUILD_FLAGS="$BUILD_FLAGS -DBUILD_EXAMPLES=ON"
-#BUILD_FLAGS="$BUILD_FLAGS -DBUILD_CFP=ON"
+BUILD_FLAGS="$BUILD_FLAGS -DBUILD_UTILITIES=OFF"
+BUILD_FLAGS="$BUILD_FLAGS -DBUILD_EXAMPLES=OFF"
+BUILD_FLAGS="$BUILD_FLAGS -DBUILD_CFP=OFF"
 #BUILD_FLAGS="$BUILD_FLAGS -DCFP_NAMESPACE=cfp2"
 
 # zfpy only built for Release builds
@@ -34,28 +26,16 @@ if [ $BUILD_TYPE == "Release" ]; then
   # $PYTHON_VERSION comes from appveyor.yml and has form XY (no dot separating major and minor versions)
   ACTIVE_PY_VERSION=${ACTIVE_PY_VERSION:0:1}${ACTIVE_PY_VERSION:2:1}
 
-  # verify python version specified in appveyor.yml matches active version
   if [ $ACTIVE_PY_VERSION != $PYTHON_VERSION ]; then
     exit 1
   fi
 
-  # add build flags for zfpy
-  BUILD_FLAGS="$BUILD_FLAGS -DBUILD_ZFPY=ON -DPYTHON_LIBRARY=\"$PYTHON_LIB_PATH\" -DPYTHON_INCLUDE_DIR=\"../$VIRTUALENV_NAME/Include\""
-
-  # cython bug on mingw-w64
-  if [ $COMPILER == "mingw-w64" ]; then
-    BUILD_FLAGS="$BUILD_FLAGS -DCMAKE_C_FLAGS=\"-DMS_WIN64\""
-  fi
+  BUILD_FLAGS="$BUILD_FLAGS -DBUILD_ZFPY=ON"
 fi
 
-#BUILD_FLAGS="$BUILD_FLAGS -DZFP_WITH_ALIGNED_ALLOC=ON"
+BUILD_FLAGS="$BUILD_FLAGS -DZFP_WITH_ALIGNED_ALLOC=OFF"
 BUILD_FLAGS="$BUILD_FLAGS -DBUILD_OPENMP=OFF"
 BUILD_FLAGS="$BUILD_FLAGS -DBUILD_CUDA=OFF"
-
-# mingw needs SH flag
-if [ $COMPILER != "msvc" ]; then
-  BUILD_FLAGS="$BUILD_FLAGS -DCMAKE_SH=CMAKE_SH-NOTFOUND"
-fi
 
 run_all "$BUILD_FLAGS"
 
