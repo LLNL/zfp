f44baa7cce5779c181b3985503c0e8d8e2237946
Peter Lindstrom <pl@llnl.gov>
2019-09-23 15:54:36 -0700

    Add 3D variable-rate arrays

---
+++ array/zfp/tile3.h
@@ -0,0 +1,148 @@
+#ifndef ZFP_TILE3_H
+#define ZFP_TILE3_H
+
+#include "tile.h"
+
+namespace zfp {
+
+// tile of 16x16x16 variable-rate 4x4x4 blocks
+template <typename Scalar>
+class Tile3 : public Tile {
+public:
+  Tile3(uint minbits = 64) :
+    Tile(minbits)
+  {}
+
+#ifdef DEBUG
+  void fragmentation(zfp_stream* zfp, FILE* file = stderr) const
+  {
+    uchar* slot = new uchar[0x10000];
+    std::fill(slot, slot + 0x10000, 0xff);
+    size_t usage = 0;
+    size_t zeros = 0;
+    // process compressed blocks
+    for (uint b = 0; b < blocks; b++) {
+      offset p = pos[b];
+      if (p == null)
+        zeros++;
+      else if (p != cached) {
+        Scalar block[block_size];
+        uint bits = decode_block(zfp, block, p);
+        uint words = word_size(bits);
+        uint s = slot_size(words);
+        usage += quantum_bytes() << s;
+        for (uint i = 0; i < (1u << s); i++)
+          slot[p + i] = s;
+      }
+    }
+    // process free slots
+    for (uint s = 0; s < sizes; s++) {
+      for (offset p = head[s]; p != null; p = get_next_slot(p))
+        for (uint i = 0; i < (1u << s); i++)
+          slot[p + i] = 0x80 + s;
+    }
+    // print 
+    for (uint i = 0; i < 0x10000; i++)
+      fprintf(file, "%02x ", slot[i]);
+    fprintf(file, "\n");
+    delete[] slot;
+    fprintf(file, "bytes=%zu vs %zu\n", bytes, usage);
+    fprintf(file, "zeros=%zu\n", zeros);
+  }
+#endif
+
+  // allocate compressed data and compress block with tile-local index 'id'
+  void compress(zfp_stream* zfp, const Scalar* block, uint id, uchar shape = 0)
+  {
+    offset p = null;
+    // compress block
+    uint bits = encode_block(zfp, block, shape);
+    // if block is empty, no storage is needed; otherwise, find space for it
+    if (bits > 1u) {
+      // allocate memory for compressed block
+      size_t words = word_size(bits);
+      p = allocate(words);
+      assert(p != null);
+      assert(p != cached);
+      // copy compressed data to persistent storage
+      const word* buffer = static_cast<const word*>(stream_data(zfp->stream));
+      std::copy(buffer, buffer + words, data + offset_words(p));
+    }
+    pos[id] = p;
+  }
+
+  // decompress block with tile-local index 'id' and free compressed data
+  void decompress(zfp_stream* zfp, Scalar* block, uint id, uchar shape = 0, bool cache = true)
+  {
+    offset p = pos[id];
+    assert(p != cached);
+    if (cache)
+      pos[id] = cached;
+    if (p == null) {
+      // empty block; fill with zeros
+      std::fill(block, block + block_size, Scalar(0));
+    }
+    else {
+      // decompress block
+      uint bits = decode_block(zfp, block, p, shape);
+      if (cache) {
+        // free space occupied by compressed data
+        size_t words = word_size(bits);
+        deallocate(p, words);
+      }
+    }
+  }
+
+  // storage class for block with given id
+  Tile::storage block_storage(zfp_stream* zfp, uint id, uchar shape = 0) const
+  {
+    offset p = pos[id];
+    if (p == null)
+      return storage_empty;
+    if (p == cached)
+      return storage_cached;
+    if (p & 1u)
+      return storage_xs;
+    Scalar block[block_size];
+    uint bits = decode_block(zfp, block, p, shape);
+    return storage(slot_size(word_size(bits)));
+  }
+
+  static const uint bx = 16;                // number of blocks per tile along x
+  static const uint by = 16;                // number of blocks per tile along y
+  static const uint bz = 16;                // number of blocks per tile along z
+  static const uint block_size = 4 * 4 * 4; // number of scalars per block
+
+protected:
+  // compress block to beginning of stream and return its storage size in bits
+  uint encode_block(zfp_stream* zfp, const Scalar* block, uchar shape = 0) const
+  {
+    // compress block to temporary storage
+    stream_rewind(zfp->stream);
+    uint bits = zfp::codec<Scalar>::encode_block_3(zfp, block, shape);
+    // if block is non-empty (i.e., stored), make sure stream is flushed
+    if (bits > 1u)
+      bits += stream_flush(zfp->stream);
+    return bits;
+  }
+
+  // decompress block stored at offset p
+  uint decode_block(zfp_stream* zfp, Scalar* block, offset p, uchar shape = 0) const
+  {
+    // save current buffer
+    void* buffer = stream_data(zfp->stream);
+    size_t size = stream_capacity(zfp->stream);
+    // decompress block to cache
+    stream_reopen(zfp->stream, data, capacity());
+    stream_rseek(zfp->stream, offset_bits(p));
+    uint bits = zfp::codec<Scalar>::decode_block_3(zfp, block, shape);
+    bits += stream_align(zfp->stream);
+    // restore buffer
+    stream_reopen(zfp->stream, buffer, size);
+    return bits;
+  }
+};
+
+}
+
+#endif
---
+++ array/zfpvarray3.h
@@ -0,0 +1,391 @@
+#ifndef ZFP_VARRAY3_H
+#define ZFP_VARRAY3_H
+
+#include <cstddef>
+#include <iterator>
+#include <cstring>
+#include "zfpvarray.h"
+#include "zfpcodec.h"
+#include "zfp/tile3.h"
+#include "zfp/cache.h"
+
+namespace zfp {
+
+// compressed 3D array of scalars
+template < typename Scalar, class Codec = zfp::codec<Scalar> >
+class varray3 : public varray {
+public:
+  // forward declarations
+  class reference;
+  class pointer;
+  class iterator;
+  #include "zfp/vreference3.h"
+  #include "zfp/vpointer3.h"
+  #include "zfp/viterator3.h"
+
+  typedef varray::storage storage;
+
+  // default constructor
+  varray3() : varray(3, Codec::type), tile(0) {}
+
+  // constructor of nx * ny * nz array using prec bits of precision, at least
+  // csize bytes of cache, and optionally initialized from flat array p
+  varray3(uint nx, uint ny, uint nz, uint prec, const Scalar* p = 0, size_t csize = 0) :
+    varray(3, Codec::type),
+    cache(lines(csize, nx, ny, nz)),
+    tile(0)
+  {
+    set_precision(prec);
+    resize(nx, ny, nz);
+    if (p)
+      set(p);
+  }
+
+  // constructor of nx * ny * nz array using error tolerance tol, at least
+  // csize bytes of cache, and optionally initialized from flat array p
+  varray3(uint nx, uint ny, uint nz, double tol, const Scalar* p = 0, size_t csize = 0) :
+    varray(3, Codec::type),
+    cache(lines(csize, nx, ny, nz)),
+    tile(0)
+  {
+    set_accuracy(tol);
+    resize(nx, ny, nz);
+    if (p)
+      set(p);
+  }
+
+  // copy constructor--performs a deep copy
+  varray3(const varray3& a) :
+    varray()
+  {
+    deep_copy(a);
+  }
+
+  // virtual destructor
+  virtual ~varray3()
+  {
+    free();
+  }
+
+  // assignment operator--performs a deep copy
+  varray3& operator=(const varray3& a)
+  {
+    if (this != &a)
+      deep_copy(a);
+    return *this;
+  }
+
+  // total number of elements in array
+  size_t size() const { return size_t(nx) * size_t(ny) * size_t(nz); }
+
+  // array dimensions
+  uint size_x() const { return nx; }
+  uint size_y() const { return ny; }
+  uint size_z() const { return nz; }
+
+  // resize the array (all previously stored data will be lost)
+  void resize(uint nx, uint ny, uint nz)
+  {
+    if (nx == 0 || ny == 0 || nz == 0)
+      free();
+    else {
+      assert(!(nx & 3u) && !(ny & 3u) && !(nz & 3u) && !"only multiples of four supported");
+      // precompute block dimensions
+      this->nx = nx;
+      this->ny = ny;
+      this->nz = nz;
+      bx = (nx + 3) / 4;
+      by = (ny + 3) / 4;
+      bz = (nz + 3) / 4;
+      blocks = bx * by * bz;
+      tx = (bx + Tile3<Scalar>::bx - 1) / Tile3<Scalar>::bx;
+      ty = (by + Tile3<Scalar>::by - 1) / Tile3<Scalar>::by;
+      tz = (bz + Tile3<Scalar>::bz - 1) / Tile3<Scalar>::bz;
+      tiles = tx * ty * tz;
+      alloc();
+    }
+  }
+
+  // cache size in number of bytes
+  size_t cache_size() const { return cache.size() * sizeof(CacheLine); }
+
+  // set minimum cache size in bytes (array dimensions must be known)
+  void set_cache_size(size_t csize)
+  {
+    flush_cache();
+    cache.resize(lines(csize, nx, ny, nz));
+  }
+
+  // empty cache without compressing modified cached blocks
+  void clear_cache() const { cache.clear(); }
+
+  // flush cache by compressing all modified cached blocks
+  void flush_cache() const
+  {
+    for (typename zfp::Cache<CacheLine>::const_iterator p = cache.first(); p; p++) {
+      if (p->tag.index() && (true || p->tag.dirty())) {
+        uint b = p->tag.index() - 1;
+        encode(b, p->line->data());
+      }
+      cache.flush(p->line);
+    }
+  }
+
+  virtual size_t storage_size(uint mask = ZFP_DATA_ALL) const
+  {
+    size_t size = varray::storage_size(mask);
+    for (uint t = 0; t < tiles; t++)
+      size += tile[t]->size(mask);
+//    if (mask & ZFP_DATA_META)
+//      size += tiles * sizeof(*tile);
+    if (mask & ZFP_DATA_META)
+      size += sizeof(varray3) - sizeof(varray);
+    if (mask & ZFP_DATA_CACHE)
+      size += cache.size() * (sizeof(CacheLine) + sizeof(typename Cache<CacheLine>::Tag));
+    return size;
+  }
+
+  storage element_storage(uint i, uint j, uint k) const
+  {
+    uint index = block(i, j, k);
+    uint t = tile_id(index);
+    uint b = block_id(index);
+    return tile[t]->block_storage(zfp, b);
+  }
+
+  // decompress array and store at p
+  void get(Scalar* p) const
+  {
+    uint index = 0;
+    for (uint z = 0; z < bz; z++, p += 4 * (ny - by))
+      for (uint y = 0; y < by; y++, p += 4 * (nx - bx))
+        for (uint x = 0; x < bx; x++, p += 4, index++) {
+          const CacheLine* line = cache.lookup(index + 1);
+          if (line)
+            line->get(p, 1, nx, nx * ny, shape ? shape[index] : 0);
+          else {
+            Scalar block[4 * 4 * 4];
+            uint t = tile_id(index);
+            uint b = block_id(index);
+            tile[t]->decompress(zfp, block, b, shape ? shape[index] : 0, false);
+            for (uint k = 0; k < 4; k++)
+              for (uint j = 0; j < 4; j++)
+                for (uint i = 0; i < 4; i++)
+                  p[i + nx * (j + ny * k)] = block[i + 4 * (j + 4 * k)];
+          }
+        }
+  }
+
+  // initialize array by copying and compressing data stored at p
+  void set(const Scalar* p)
+  {
+#if 0
+    uint b = 0;
+    for (uint j = 0; j < by; j++, p += 4 * (nx - bx))
+      for (uint i = 0; i < bx; i++, p += 4, b++)
+        encode(b, p, 1, nx); // error: no strided encode
+    cache.clear();
+#endif
+//#warning "set not implemented"
+    (void)p;
+    abort();
+  }
+
+  // (i, j, k) accessors
+  Scalar operator()(uint i, uint j, uint k) const { return get(i, j, k); }
+  reference operator()(uint i, uint j, uint k) { return reference(this, i, j, k); }
+
+  // flat index accessors
+  Scalar operator[](uint index) const
+  {
+    uint i, j, k;
+    ijk(i, j, k, index);
+    return get(i, j, k);
+  }
+  reference operator[](uint index)
+  {
+    uint i, j, k;
+    ijk(i, j, k, index);
+    return reference(this, i, j, k);
+  }
+
+  // sequential iterators
+  iterator begin() { return iterator(this, 0, 0, 0); }
+  iterator end() { return iterator(this, 0, 0, nz); }
+
+protected:
+  // cache line representing one block of decompressed values
+  class CacheLine {
+  public:
+    Scalar operator()(uint i, uint j, uint k) const { return a[index(i, j, k)]; }
+    Scalar& operator()(uint i, uint j, uint k) { return a[index(i, j, k)]; }
+    const Scalar* data() const { return a; }
+    Scalar* data() { return a; }
+    // copy cache line
+    void get(Scalar* p, int sx, int sy, int sz) const
+    {
+      const Scalar* q = a;
+      for (uint z = 0; z < 4; z++, p += sz - 4 * sy)
+        for (uint y = 0; y < 4; y++, p += sy - 4 * sx)
+          for (uint x = 0; x < 4; x++, p += sx, q++)
+            *p = *q;
+    }
+    void get(Scalar* p, int sx, int sy, int sz, uint shape) const
+    {
+      if (!shape)
+        get(p, sx, sy, sz);
+      else {
+        // determine block dimensions
+        uint nx = 4 - (shape & 3u); shape >>= 2;
+        uint ny = 4 - (shape & 3u); shape >>= 2;
+        uint nz = 4 - (shape & 3u); shape >>= 2;
+        const Scalar* q = a;
+        for (uint z = 0; z < nz; z++, p += sz - (ptrdiff_t)ny * sy, q += 16 - 4 * ny)
+          for (uint y = 0; y < ny; y++, p += sy - (ptrdiff_t)nx * sx, q += 4 - nx)
+            for (uint x = 0; x < nx; x++, p += sx, q++)
+              *p = *q;
+      }
+    }
+  protected:
+    static uint index(uint i, uint j, uint k) { return (i & 3u) + 4 * ((j & 3u) + 4 * (k & 3u)); }
+    Scalar a[64];
+  };
+
+  // allocate memory for compressed data
+  void alloc()
+  {
+    varray::alloc();
+    if (tile) {
+      for (uint t = 0; t < tiles; t++)
+        delete tile[t];
+      delete[] tile;
+    }
+    tile = new Tile3<Scalar>*[tiles];
+    for (uint t = 0; t < tiles; t++)
+      tile[t] = new Tile3<Scalar>(minbits);
+  }
+
+  // free memory associated with compressed data
+  void free()
+  {
+    for (uint t = 0; t < tiles; t++)
+      delete tile[t];
+    delete[] tile;
+    tile = 0;
+    varray::free();
+  }
+
+  // perform a deep copy
+  void deep_copy(const varray3& a)
+  {
+    // copy base class members
+    varray::deep_copy(a);
+    // copy cache
+    cache = a.cache;
+  }
+
+  // inspector
+  Scalar get(uint i, uint j, uint k) const
+  {
+    const CacheLine* p = line(i, j, k, false);
+    return (*p)(i, j, k);
+  }
+
+  // mutator
+  void set(uint i, uint j, uint k, Scalar val)
+  {
+    CacheLine* p = line(i, j, k, true);
+    (*p)(i, j, k) = val;
+  }
+
+  // in-place updates
+  void add(uint i, uint j, uint k, Scalar val) { (*line(i, j, k, true))(i, j, k) += val; }
+  void sub(uint i, uint j, uint k, Scalar val) { (*line(i, j, k, true))(i, j, k) -= val; }
+  void mul(uint i, uint j, uint k, Scalar val) { (*line(i, j, k, true))(i, j, k) *= val; }
+  void div(uint i, uint j, uint k, Scalar val) { (*line(i, j, k, true))(i, j, k) /= val; }
+
+  // return cache line for (i, j, k); may require write-back and fetch
+  CacheLine* line(uint i, uint j, uint k, bool write) const
+  {
+    CacheLine* p = 0;
+    uint b = block(i, j, k);
+    typename zfp::Cache<CacheLine>::Tag t = cache.access(p, b + 1, write);
+    uint c = t.index() - 1;
+    if (c != b) {
+      // write back occupied cache line (done even if not dirty)
+      if (t.index())
+        encode(c, p->data());
+      // fetch cache line
+      decode(b, p->data());
+    }
+    return p;
+  }
+
+  // encode block with given index
+  void encode(uint index, const Scalar* block) const
+  {
+    uint t = tile_id(index);
+    uint b = block_id(index);
+    tile[t]->compress(zfp, block, b, shape ? shape[index] : 0);
+  }
+
+  // decode block with given index
+  void decode(uint index, Scalar* block) const
+  {
+    uint t = tile_id(index);
+    uint b = block_id(index);
+    tile[t]->decompress(zfp, block, b, shape ? shape[index] : 0);
+  }
+
+  // block index for (i, j, k)
+  uint block(uint i, uint j, uint k) const { return (i / 4) + bx * ((j / 4) + by * (k / 4)); }
+
+  // tile id associated with given block index
+  uint tile_id(uint block) const
+  {
+    uint x = block % bx; block /= bx;
+    uint y = block % by; block /= by;
+    uint z = block;
+    uint xx = x / Tile3<Scalar>::bx;
+    uint yy = y / Tile3<Scalar>::by;
+    uint zz = z / Tile3<Scalar>::bz;
+    return xx + tx * (yy + ty * zz);
+  }
+
+  // tile-local block id associated with given global block index
+  uint block_id(uint block) const
+  {
+    uint x = block % bx; block /= bx;
+    uint y = block % by; block /= by;
+    uint z = block;
+    uint xx = x % Tile3<Scalar>::bx;
+    uint yy = y % Tile3<Scalar>::by;
+    uint zz = z % Tile3<Scalar>::bz;
+    return xx + Tile3<Scalar>::bx * (yy + Tile3<Scalar>::by * zz);
+  }
+
+  // convert flat index to (i, j)
+  void ijk(uint& i, uint& j, uint& k, uint index) const
+  {
+    i = index % nx; index /= nx;
+    j = index % ny; index /= ny;
+    k = index;
+  }
+
+  // number of cache lines corresponding to size (or suggested size if zero)
+  static uint lines(size_t size, uint nx, uint ny, uint nz)
+  {
+    uint n = size ? uint((size + sizeof(CacheLine) - 1) / sizeof(CacheLine)) : varray::lines(size_t((nx + 3) / 4) * size_t((ny + 3) / 4) * size_t((nz + 3) / 4));
+    return std::max(n, 1u);
+  }
+
+  mutable zfp::Cache<CacheLine> cache; // cache of decompressed blocks
+  Tile3<Scalar>** tile;                // tiles of compressed blocks
+};
+
+typedef varray3<float> varray3f;
+typedef varray3<double> varray3d;
+
+}
+
+#endif
