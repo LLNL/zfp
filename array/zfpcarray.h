e9f4743cdb58548bb3d0f7f39b96e6ff5a61b66e
lindstro <pl@llnl.gov>
2019-11-20 14:15:51 -0700

    Initial pass for 3D arrays

---
+++ array/zfpcarray.h
@@ -0,0 +1,177 @@
+#ifndef ZFP_CARRAY_H
+#define ZFP_CARRAY_H
+
+#include <algorithm>
+#include <climits>
+#include <cstring>
+#include <stdexcept>
+#include <string>
+
+#include "zfp.h"
+#include "zfp/memory.h"
+
+namespace zfp {
+
+// abstract base class for compressed array of scalars
+class const_array {
+protected:
+  // default constructor
+  const_array() :
+    dims(0), type(zfp_type_none),
+    nx(0), ny(0), nz(0),
+    bx(0), by(0), bz(0),
+    blocks(0), blkbits(0),
+    bytes(0), data(0),
+    zfp(0),
+    shape(0)
+  {}
+
+  // generic array with 'dims' dimensions and scalar type 'type'
+  const_array(uint dims, zfp_type type) :
+    dims(dims), type(type),
+    nx(0), ny(0), nz(0),
+    bx(0), by(0), bz(0),
+    blocks(0), blkbits(0),
+    bytes(0), data(0),
+    zfp(zfp_stream_open(0)),
+    shape(0)
+  {}
+
+  // copy constructor--performs a deep copy
+  const_array(const const_array& a) :
+    data(0),
+    zfp(0),
+    shape(0)
+  {
+    deep_copy(a);
+  }
+
+  // assignment operator--performs a deep copy
+  const_array& operator=(const const_array& a)
+  {
+    deep_copy(a);
+    return *this;
+  }
+
+public:
+  // public virtual destructor (can delete array through base class pointer)
+  virtual ~const_array()
+  {
+    free();
+    zfp_stream_close(zfp);
+  }
+
+  // rate in bits per value
+  double rate() const { return double(blkbits) / block_size(); }
+
+  // set compression rate in bits per value
+  double set_rate(double rate)
+  {
+    rate = zfp_stream_set_rate(zfp, rate, type, dims, 1);
+    blkbits = zfp->maxbits;
+    alloc();
+    return rate;
+  }
+
+  // empty cache without compressing modified cached blocks
+  virtual void clear_cache() const = 0;
+
+  // number of bytes of compressed data
+  size_t compressed_size() const { return bytes; }
+
+  // pointer to compressed data for read or write access
+  uchar* compressed_data() const { return data; }
+
+  // dimensionality
+  uint dimensionality() const { return dims; }
+
+  // underlying scalar type
+  zfp_type scalar_type() const { return type; }
+
+protected:
+  // number of values per block
+  uint block_size() const { return 1u << (2 * dims); }
+
+  // allocate memory for compressed data
+  void alloc(bool clear = true)
+  {
+abort();
+    bytes = blocks * blkbits / CHAR_BIT;
+    zfp::reallocate_aligned(data, bytes, 0x100u);
+    if (clear)
+      std::fill(data, data + bytes, 0);
+    stream_close(zfp->stream);
+    zfp_stream_set_bit_stream(zfp, stream_open(data, bytes));
+    clear_cache();
+  }
+
+  // free memory associated with compressed data
+  void free()
+  {
+abort();
+    nx = ny = nz = 0;
+    bx = by = bz = 0;
+    blocks = 0;
+    stream_close(zfp->stream);
+    zfp_stream_set_bit_stream(zfp, 0);
+    bytes = 0;
+    zfp::deallocate_aligned(data);
+    data = 0;
+    zfp::deallocate(shape);
+    shape = 0;
+  }
+
+  // perform a deep copy
+  void deep_copy(const const_array& a)
+  {
+abort();
+    // copy metadata
+    dims = a.dims;
+    type = a.type;
+    nx = a.nx;
+    ny = a.ny;
+    nz = a.nz;
+    bx = a.bx;
+    by = a.by;
+    bz = a.bz;
+    blocks = a.blocks;
+    blkbits = a.blkbits;
+    bytes = a.bytes;
+
+    // copy dynamically allocated data
+    zfp::clone_aligned(data, a.data, bytes, 0x100u);
+    if (zfp) {
+      if (zfp->stream)
+        stream_close(zfp->stream);
+      zfp_stream_close(zfp);
+    }
+    zfp = zfp_stream_open(0);
+    *zfp = *a.zfp;
+    zfp_stream_set_bit_stream(zfp, stream_open(data, bytes));
+    zfp::clone(shape, a.shape, blocks);
+  }
+
+  // default number of cache lines for array with n blocks
+  static uint lines(size_t n)
+  {
+    // compute m = O(sqrt(n))
+    size_t m;
+    for (m = 1; m * m < n; m *= 2);
+    return static_cast<uint>(m);
+  }
+
+  uint dims;           // array dimensionality (1, 2, or 3)
+  zfp_type type;       // scalar type
+  uint nx, ny, nz;     // array dimensions
+  uint bx, by, bz;     // array dimensions in number of blocks
+  uint blocks;         // number of blocks
+  size_t blkbits;      // number of bits per compressed block
+  size_t bytes;        // total bytes of compressed data
+  mutable uchar* data; // pointer to compressed data
+  zfp_stream* zfp;     // compressed stream of blocks
+  uchar* shape;        // precomputed block dimensions (or null if uniform)
+};
+
+}
+
+#endif
---
+++ array/zfpcarray3.h
@@ -0,0 +1,318 @@
+#ifndef ZFP_CARRAY3_H
+#define ZFP_CARRAY3_H
+
+#include <cstddef>
+#include <iterator>
+#include <cstring>
+#include "zfpcarray.h"
+#include "zfpcodec.h"
+#include "zfp/cache.h"
+
+namespace zfp {
+
+// compressed 3D array of scalars
+template < typename Scalar, class Codec = zfp::codec<Scalar> >
+class const_array3 : public const_array {
+public:
+#if 0
+  // forward declarations
+  class reference;
+  class pointer;
+  class iterator;
+  class view;
+  #include "zfp/reference3.h"
+  #include "zfp/pointer3.h"
+  #include "zfp/iterator3.h"
+  #include "zfp/view3.h"
+#endif
+
+  // default constructor
+  const_array3() : const_array(3, Codec::type) {}
+
+  // constructor of nx * ny * nz array using rate bits per value, at least
+  // csize bytes of cache, and optionally initialized from flat array p
+  const_array3(uint nx, uint ny, uint nz, double rate, const Scalar* p = 0, size_t csize = 0) :
+    const_array(3, Codec::type),
+    cache(lines(csize, nx, ny, nz))
+  {
+    set_rate(rate);
+    resize(nx, ny, nz, p == 0);
+    if (p)
+      set(p);
+  }
+
+#if 0
+  // constructor, from previously-serialized compressed array
+  array3(const zfp::array::header& h, const uchar* buffer = 0, size_t buffer_size_bytes = 0) :
+    array(3, Codec::type, h, buffer_size_bytes)
+  {
+    resize(nx, ny, nz, false);
+    if (buffer)
+      memcpy(data, buffer, bytes);
+  }
+
+  // copy constructor--performs a deep copy
+  array3(const array3& a) :
+    array()
+  {
+    deep_copy(a);
+  }
+
+  // construction from view--perform deep copy of (sub)array
+  template <class View>
+  array3(const View& v) :
+    array(3, Codec::type),
+    cache(lines(0, v.size_x(), v.size_y(), v.size_z()))
+  {
+    set_rate(v.rate());
+    resize(v.size_x(), v.size_y(), v.size_z(), true);
+    // initialize array in its preferred order
+    for (iterator it = begin(); it != end(); ++it)
+      *it = v(it.i(), it.j(), it.k());
+  }
+#endif
+
+  // virtual destructor
+  virtual ~const_array3() {}
+
+  // assignment operator--performs a deep copy
+  array3& operator=(const array3& a)
+  {
+    if (this != &a)
+      deep_copy(a);
+    return *this;
+  }
+
+  // total number of elements in array
+  size_t size() const { return size_t(nx) * size_t(ny) * size_t(nz); }
+
+  // array dimensions
+  uint size_x() const { return nx; }
+  uint size_y() const { return ny; }
+  uint size_z() const { return nz; }
+
+#if 0
+  // resize the array (all previously stored data will be lost)
+  void resize(uint nx, uint ny, uint nz, bool clear = true)
+  {
+    if (nx == 0 || ny == 0 || nz == 0)
+      free();
+    else {
+      this->nx = nx;
+      this->ny = ny;
+      this->nz = nz;
+      bx = (nx + 3) / 4;
+      by = (ny + 3) / 4;
+      bz = (nz + 3) / 4;
+      blocks = bx * by * bz;
+      alloc(clear);
+
+      // precompute block dimensions
+      zfp::deallocate(shape);
+      if ((nx | ny | nz) & 3u) {
+        shape = (uchar*)zfp::allocate(blocks);
+        uchar* p = shape;
+        for (uint k = 0; k < bz; k++)
+          for (uint j = 0; j < by; j++)
+            for (uint i = 0; i < bx; i++)
+              *p++ = (i == bx - 1 ? -nx & 3u : 0) + 4 * ((j == by - 1 ? -ny & 3u : 0) + 4 * (k == bz - 1 ? -nz & 3u : 0));
+      }
+      else
+        shape = 0;
+    }
+  }
+#endif
+
+  // cache size in number of bytes
+  size_t cache_size() const { return cache.size() * sizeof(CacheLine); }
+
+  // set minimum cache size in bytes (array dimensions must be known)
+  void set_cache_size(size_t csize)
+  {
+    flush_cache();
+    cache.resize(lines(csize, nx, ny, nz));
+  }
+
+  // empty cache without compressing modified cached blocks
+  void clear_cache() const { cache.clear(); }
+
+  // decompress array and store at p
+  void get(Scalar* p) const
+  {
+    uint b = 0;
+    for (uint k = 0; k < bz; k++, p += 4 * nx * (ny - by))
+      for (uint j = 0; j < by; j++, p += 4 * (nx - bx))
+        for (uint i = 0; i < bx; i++, p += 4, b++) {
+          const CacheLine* line = cache.lookup(b + 1);
+          if (line)
+            line->get(p, 1, nx, nx * ny, shape ? shape[b] : 0);
+          else
+            decode(b, p, 1, nx, nx * ny);
+        }
+  }
+
+  // initialize array by copying and compressing data stored at p
+  void set(const Scalar* p)
+  {
+    uint b = 0;
+    for (uint k = 0; k < bz; k++, p += 4 * nx * (ny - by))
+      for (uint j = 0; j < by; j++, p += 4 * (nx - bx))
+        for (uint i = 0; i < bx; i++, p += 4, b++)
+#error here
+          encode(b, p, 1, nx, nx * ny);
+    stream_flush(zfp->stream); // flush final block
+    cache.clear();
+  }
+
+  // (i, j, k) accessors
+  Scalar operator()(uint i, uint j, uint k) const { return get(i, j, k); }
+
+  // flat index corresponding to (i, j, k)
+  uint index(uint i, uint j, uint k) const { return i + nx * (j + ny * k); }
+
+  // flat index accessors
+  Scalar operator[](uint index) const
+  {
+    uint i, j, k;
+    ijk(i, j, k, index);
+    return get(i, j, k);
+  }
+
+#if 0
+  // sequential iterators
+  iterator begin() { return iterator(this, 0, 0, 0); }
+  iterator end() { return iterator(this, 0, 0, nz); }
+#endif
+
+protected:
+  // cache line representing one block of decompressed values
+  class CacheLine {
+  public:
+    Scalar operator()(uint i, uint j, uint k) const { return a[index(i, j, k)]; }
+    const Scalar* data() const { return a; }
+    // copy cache line
+    void get(Scalar* p, int sx, int sy, int sz) const
+    {
+      const Scalar* q = a;
+      for (uint z = 0; z < 4; z++, p += sz - 4 * sy)
+        for (uint y = 0; y < 4; y++, p += sy - 4 * sx)
+          for (uint x = 0; x < 4; x++, p += sx, q++)
+            *p = *q;
+    }
+    void get(Scalar* p, int sx, int sy, int sz, uint shape) const
+    {
+      if (!shape)
+        get(p, sx, sy, sz);
+      else {
+        // determine block dimensions
+        uint nx = 4 - (shape & 3u); shape >>= 2;
+        uint ny = 4 - (shape & 3u); shape >>= 2;
+        uint nz = 4 - (shape & 3u); shape >>= 2;
+        const Scalar* q = a;
+        for (uint z = 0; z < nz; z++, p += sz - (ptrdiff_t)ny * sy, q += 16 - 4 * ny)
+          for (uint y = 0; y < ny; y++, p += sy - (ptrdiff_t)nx * sx, q += 4 - nx)
+            for (uint x = 0; x < nx; x++, p += sx, q++)
+              *p = *q;
+      }
+    }
+  protected:
+    static uint index(uint i, uint j, uint k) { return (i & 3u) + 4 * ((j & 3u) + 4 * (k & 3u)); }
+    Scalar a[64];
+  };
+
+#if 0
+  // perform a deep copy
+  void deep_copy(const array3& a)
+  {
+    // copy base class members
+    array::deep_copy(a);
+    // copy cache
+    cache = a.cache;
+  }
+#endif
+
+  // inspector
+  Scalar get(uint i, uint j, uint k) const
+  {
+    const CacheLine* p = line(i, j, k, false);
+    return (*p)(i, j, k);
+  }
+
+  // return cache line for (i, j, k); may require write-back and fetch
+  const CacheLine* line(uint i, uint j, uint k) const
+  {
+    CacheLine* p = 0;
+    uint b = block(i, j, k);
+    typename zfp::Cache<CacheLine>::Tag t = cache.access(p, b + 1, false);
+    uint c = t.index() - 1;
+    if (c != b) {
+/*
+      // write back occupied cache line if it is dirty
+      if (t.dirty())
+        encode(c, p->data());
+*/
+      // fetch cache line
+      decode(b, p->data());
+    }
+    return p;
+  }
+
+  // encode block with given index
+  void encode(uint index, const Scalar* block) const
+  {
+    stream_wseek(zfp->stream, index * blkbits);
+    Codec::encode_block_3(zfp, block, shape ? shape[index] : 0);
+    stream_flush(zfp->stream);
+  }
+
+  // encode block with given index from strided array
+  void encode(uint index, const Scalar* p, int sx, int sy, int sz) const
+  {
+    stream_wseek(zfp->stream, index * blkbits);
+    Codec::encode_block_strided_3(zfp, p, shape ? shape[index] : 0, sx, sy, sz);
+    stream_flush(zfp->stream);
+  }
+
+  // decode block with given index
+  void decode(uint index, Scalar* block) const
+  {
+    stream_rseek(zfp->stream, index * blkbits);
+    Codec::decode_block_3(zfp, block, shape ? shape[index] : 0);
+  }
+
+  // decode block with given index to strided array
+  void decode(uint index, Scalar* p, int sx, int sy, int sz) const
+  {
+    stream_rseek(zfp->stream, index * blkbits);
+    Codec::decode_block_strided_3(zfp, p, shape ? shape[index] : 0, sx, sy, sz);
+  }
+
+  // block index for (i, j, k)
+  uint block(uint i, uint j, uint k) const { return (i / 4) + bx * ((j / 4) + by * (k / 4)); }
+
+  // convert flat index to (i, j, k)
+  void ijk(uint& i, uint& j, uint& k, uint index) const
+  {
+    i = index % nx;
+    index /= nx;
+    j = index % ny;
+    index /= ny;
+    k = index;
+  }
+
+  // number of cache lines corresponding to size (or suggested size if zero)
+  static uint lines(size_t size, uint nx, uint ny, uint nz)
+  {
+    uint n = size ? (size + sizeof(CacheLine) - 1) / sizeof(CacheLine) : array::lines(size_t((nx + 3) / 4) * size_t((ny + 3) / 4) * size_t((nz + 3) / 4));
+    return std::max(n, 1u);
+  }
+
+  mutable zfp::Cache<CacheLine> cache; // cache of decompressed blocks
+};
+
+typedef const_array3<float> const_array3f;
+typedef const_array3<double> const_array3d;
+
+}
+
+#endif
