e748a9c96b77d366b982c6500f8ac0cd777ad11a
lindstro <pl@llnl.gov>
2019-11-23 10:25:44 -1000

    Clean up code

--- array/zfpcarray3.h
+++ array/zfpcarray3.h
@@ -164,19 +164,13 @@ public:
       for (uint j = 0; j < by; j++, p += 4 * (nx - bx))
         for (uint i = 0; i < bx; i++, p += 4, b++) {
           size_t size = encode(b, p, 1, nx, nx * ny);
-#if DEBUG
-    fprintf(stderr, "size=%zu\n", size);
-#endif
           block_index.push(size);
         }
     // flush final block
     block_index.flush();
-//for (uint i = 0; i < blocks; i++)
-//printf("block=%i offset=%lu\n", i, block_index(i));
-//exit(0);
     stream_flush(zfp->stream);
     bytes = stream_size(zfp->stream);
-#warning "should reallocate memory here"
+//#warning "should reallocate memory here"
     cache.clear();
   }
 
@@ -201,6 +195,8 @@ public:
 #endif
 
 protected:
+  #include "zfp/index3.h"
+
   // cache line representing one block of decompressed values
   class CacheLine {
   public:
@@ -238,157 +234,6 @@ protected:
   };
 
 #if 0
-  // uncompressed index
-  class Index { // templetize?
-  public:
-    // constructor for given nbumber of blocks
-    Index(uint blocks) :
-      data(0),
-      block(0) 
-    {
-      resize(blocks);
-    }
-
-    // reset index
-    void clear() { block = 0; }
-
-    void resize(uint blocks)
-    {
-      delete[] data;
-      data = new uint64[blocks + 1];
-      clear();
-    }
-
-    // push block bit size
-    void push(size_t size)
-    {
-      if (block)
-        data[block + 1] = data[block] + size;
-      else {
-        data[0] = 0;
-        data[1] = size;
-      }
-      block++;
-    }
-
-    // flush any buffered data
-    void flush() {}
-
-    // bit offset of given block id
-    size_t operator()(uint id) const { return data[id]; }
-
-  protected:
-    uint64* data;
-    uint block;
-  };
-#else
-  // compressed index
-  class Index { // templetize?
-  public:
-    // constructor for given nbumber of blocks
-    Index(uint blocks) :
-      data(0),
-      block(0) 
-    {
-      resize(blocks);
-    }
-
-    // reset index
-    void clear()
-    {
-      block = 0;
-      ptr = 0;
-    }
-
-    void resize(uint blocks)
-    {
-      delete[] data;
-      data = new uint64[2 * ((blocks + 7) / 8)];
-      clear();
-    }
-
-    // push block bit size
-    void push(size_t size)
-    {
-      uint chunk = block / 8;
-      buffer[block & 0x7u] = size;
-      block++;
-      if (!(block & 0x7u)) {
-        // store all but low 8-bits of offset
-        uint64 hi = (ptr >> 8) << 28;
-        uint64 lo = (ptr & UINT64C(0xff)) << 56;
-        for (uint k = 0; k < 7; k++) {
-          // partition block size into 4 high and 8 low bits
-          hi += (buffer[k] >> 8) << (4 * (6 - k));
-          lo += (buffer[k] & 0xffu) << (8 * (6 - k));
-          ptr += buffer[k];
-        }
-//fprintf(stderr, "hi=%016lx lo=%016lx\n", hi, lo);
-        ptr += buffer[7];
-        data[2 * chunk + 0] = hi;
-        data[2 * chunk + 1] = lo;
-      }
-    }
-
-    // flush any buffered data
-    void flush()
-    {
-      while (block & 0x7u)
-        push(0);
-    }
-
-    // bit offset of given block id
-    size_t operator()(uint id) const
-    {
-      uint chunk = id / 8;
-      uint which = id % 8;
-//fprintf(stderr, "offset(%u) ", id);
-      return offset(data[2 * chunk + 0], data[2 * chunk + 1], which);
-    }
-
-  protected:
-    // kth offset in chunk, 0 <= k <= 7
-    static uint64 offset(uint64 h, uint64 l, uint k)
-    {
-//fprintf(stderr, "(%016lx %016lx %u) ", h, l, k);
-      uint64 base = h >> 32;
-      h &= UINT64C(0xffffffff);
-      uint64 hi = sum4(h >> (4 * (7 - k)));
-      uint64 lo = sum8(l >> (8 * (7 - k)));
-//fprintf(stderr, "(%016lx %016lx %016lx)\n", base, hi, lo);
-      return (base << 12) + (hi << 8) + lo;
-    }
-
-    // sum of eight packed 4-bit numbers
-    static uint64 sum4(uint64 x)
-    {
-      uint64 y = x & UINT64C(0xf0f0f0f0);
-      x -= y;
-      x += y >> 4;
-      x += x >> 16;
-      x += x >> 8;
-      return x & UINT64C(0xff);
-    }
-  
-    // sum of eight packed 8-bit numbers
-    static uint64 sum8(uint64 x)
-    {
-      uint64 y = x & UINT64C(0xff00ff00ff00ff00);
-      x -= y;
-      x += y >> 8;
-      x += x >> 32;
-      x += x >> 16;
-      return x & UINT64C(0xffff);
-    }
-
-    uint64* data;
-    uint block;
-    uint64 ptr;
-    size_t buffer[8];
-  };
-#endif
-
-#if 0
   // perform a deep copy
   void deep_copy(const array3& a)
   {
@@ -413,15 +258,9 @@ protected:
     uint b = block(i, j, k);
     typename zfp::Cache<CacheLine>::Tag t = cache.access(p, b + 1, false);
     uint c = t.index() - 1;
-    if (c != b) {
-/*
-      // write back occupied cache line if it is dirty
-      if (t.dirty())
-        encode(c, p->data());
-*/
-      // fetch cache line
+    // fetch cache line if not cached
+    if (c != b)
       decode(b, p->data());
-    }
     return p;
   }
 
@@ -472,7 +311,7 @@ protected:
   }
 
   mutable zfp::Cache<CacheLine> cache; // cache of decompressed blocks
-  Index block_index; // block index
+  Hybrid8Index block_index; // block index
 };
 
 typedef const_array3<float> const_array3f;
