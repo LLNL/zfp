8d0f76fd2d31d899f11ad7bf3312e22abccab038
David Wade <dawad@equinor.com>
2020-03-30 07:09:21 +0000

    Windows friendly setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     url="https://computing.llnl.gov/projects/floating-point-compression",
     description="zfp compression in Python",
     long_description="zfp compression in Python",
-    ext_modules = [Extension('zfpy', ['build/python/zfpy.c'], 
-                             include_dirs=['include', np.get_include()], 
-                             libraries=['zfp'], library_dirs=['build/lib64'])]
+    ext_modules=[Extension('zfpy', ['build/python/zfpy.c'],
+                           include_dirs=['include', np.get_include()],
+                           libraries=['zfp'], library_dirs=['build/lib64', 'build/lib/Release'])]
 )
