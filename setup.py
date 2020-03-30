ba69c2f26294a102312845e286038b8d664e9955
David Wade <dawad@equinor.com>
2020-03-30 19:39:01 +0000

    Update version

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ import numpy as np
 
 setup(
     name="zfpy",
-    version="0.5.5",
+    version="0.5.5-rc1",
     author="Peter Lindstrom",
     author_email="zfp@llnl.gov",
     url="https://computing.llnl.gov/projects/floating-point-compression",
     description="zfp compression in Python",
     long_description="zfp compression in Python",
-    ext_modules=[Extension('zfpy', ['build/python/zfpy.c'],
-                           include_dirs=['include', np.get_include()],
-                           libraries=['zfp'], library_dirs=['build/lib64', 'build/lib/Release'])]
+    ext_modules=[Extension("zfpy", ["build/python/zfpy.c"],
+                           include_dirs=["include", np.get_include()],
+                           libraries=["zfp"], library_dirs=["build/lib64", "build/lib/Release"])]
 )
