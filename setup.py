cf8740a1d9b8cc54f702889febee6ec785735480
lindstro <pl@llnl.gov>
2020-05-26 07:57:44 -0700

    zfp 0.5.5-rc4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import numpy as np
 
 setup(
     name="zfpy",
-    version="0.5.5-rc3",
+    version="0.5.5-rc4",
     author="Peter Lindstrom",
     author_email="zfp@llnl.gov",
     url="https://computing.llnl.gov/projects/floating-point-compression",
