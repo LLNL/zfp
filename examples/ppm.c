8b794fb8b01f2a51aca434b6b39eede8257a327a
lindstro <pl@llnl.gov>
2020-08-25 14:57:49 -0700

    Fix ppm issues with clamping; add chroma dimensionality parameter

--- examples/ppm.c
+++ examples/ppm.c
@@ -1,4 +1,38 @@
-/* simple example that shows how zfp can be used to compress ppm color images */
+/*
+This simple example shows how zfp can be used to compress 8-bit color images
+stored in the PPM image format.  This lossy compressor employs two common image
+compression strategies: (1) transformation to the YCoCg color space, which
+decorrelates color bands, and (2) chroma subsampling, which reduces spatial
+resolution in the Co and Cg chrominance bands.  The single command-line argument
+selects one of two compression modes: if a positive rate (in bits/pixel) is
+specified, fixed-rate mode is selected; a negative integer argument, -p, sets
+the precision to p in fixed-precision mode.  Rate allocation in fixed-rate mode
+assigns more bits to luma than to chroma components due to the relatively higher
+information content in luma after chroma subsampling.
+
+The YCoCg transform employed here has been adapted to avoid range expansion and
+potential overflow.  Chroma subsampling is achieved by performing zfp's forward
+decorrelating transform and then zeroing all but the four lowest-sequency
+coefficients, effectively reducing each chroma block to a bilinear approximation.
+
+Because only four chroma coefficients per 4x4 pixel block are retained, an
+alternative to zeroing and then encoding the remaining twelve zero-valued
+coefficients is to treat the chroma block as being one-dimensional, with only
+four values, and then compressing it using zfp's 1D codec.  The dimensionality
+of chroma blocks (1 or 2) is specified at compile time via the PPM_CHROMA macro.
+
+NOTE: To keep this example simple, only images whose dimensions are multiples
+of four are supported.
+*/
+
+#ifdef PPM_CHROMA
+  #if PPM_CHROMA != 1 && PPM_CHROMA != 2
+    #error "compile with PPM_CHROMA=1 or PPM_CHROMA=2"
+  #endif
+#else
+  /* default */
+  #define PPM_CHROMA 2
+#endif
 
 #include <limits.h>
 #include <math.h>
@@ -7,7 +41,18 @@
 #include <string.h>
 #include "zfp.h"
 
-#define CHROMA2D // treat 4-coefficient chroma blocks as 2D
+/* clamp values to 31-bit range */
+static void
+clamp(int32* block, uint n)
+{
+  uint i;
+  for (i = 0; i < n; i++) {
+    if (block[i] < 1 - (1 << 30))
+      block[i] = 1 - (1 << 30);
+    if (block[i] > (1 << 30) - 1)
+      block[i] = (1 << 30) - 1;
+  }
+}
 
 /* convert 2D block from RGB to YCoCg color space */
 static void
@@ -111,7 +156,17 @@ chroma_downsample(int32* block)
     fwd_lift(block + 4 * j, 1);
   for (i = 0; i < 4; i++)
     fwd_lift(block + 1 * i, 4);
-#ifdef CHROMA2D
+#if PPM_CHROMA == 1
+  /* keep only the four lowest-sequency coefficients */
+  block[2] = block[4];
+  block[3] = block[5];
+  for (i = 4; i < 16; i++)
+    block[i] = 0;
+  /* reconstruct as 1D block */
+  inv_lift(block, 1);
+  /* clamp values to 31 bits to avoid overflow */
+  clamp(block, 4);
+#else
   /* zero out all but four lowest-sequency coefficients */
   for (j = 0; j < 4; j++)
     for (i = 0; i < 4; i++)
@@ -122,22 +177,16 @@ chroma_downsample(int32* block)
     inv_lift(block + 1 * i, 4);
   for (j = 0; j < 4; j++)
     inv_lift(block + 4 * j, 1);
-#else
-  /* keep only the four lowest-sequency coefficients */
-  block[2] = block[4];
-  block[3] = block[5];
-  for (i = 4; i < 16; i++)
-    block[i] = 0;
-  /* reconstruct as 1D block */
-  inv_lift(block, 1);
+  /* clamp values to 31 bits to avoid overflow */
+  clamp(block, 16);
 #endif
 }
 
-#ifndef CHROMA2D
-/* reconstruct 2D chroma block from 1D block */
+/* reconstruct 2D chroma block */
 static void
 chroma_upsample(int32* block)
 {
+#if PPM_CHROMA == 1
   uint i, j;
   /* obtain 1D block coefficients */
   fwd_lift(block, 1);
@@ -153,8 +202,13 @@ chroma_upsample(int32* block)
     inv_lift(block + 1 * i, 4);
   for (j = 0; j < 4; j++)
     inv_lift(block + 4 * j, 1);
-}
+  /* clamp values to 31 bits to avoid overflow */
+  clamp(block, 16);
+#else
+  /* clamp values to 31 bits to avoid overflow */
+  clamp(block, 16);
 #endif
+}
 
 int main(int argc, char* argv[])
 {
@@ -189,7 +243,6 @@ int main(int argc, char* argv[])
     fprintf(stderr, "error opening image\n");
     return EXIT_FAILURE;
   }
-
   if ((nx & 3u) || (ny & 3u)) {
     fprintf(stderr, "image dimensions must be multiples of four\n");
     return EXIT_FAILURE;
@@ -216,18 +269,18 @@ int main(int argc, char* argv[])
   }
   else {
     /* assign higher rate to luminance than to chrominance components */
-#ifdef CHROMA2D
-    double chroma_rate = floor(8 * rate / 3 + 0.5) / 16;
-    double luma_rate = rate - 2 * chroma_rate;
-    zfp_stream_set_rate(zfp[0], luma_rate, zfp_type_int32, 2, 0);
-    zfp_stream_set_rate(zfp[1], chroma_rate, zfp_type_int32, 2, 0);
-    zfp_stream_set_rate(zfp[2], chroma_rate, zfp_type_int32, 2, 0);
-#else
+#if PPM_CHROMA == 1
     double chroma_rate = floor(8 * rate / 3 + 0.5) / 4;
     double luma_rate = rate - chroma_rate / 2;
     zfp_stream_set_rate(zfp[0], luma_rate, zfp_type_int32, 2, 0);
     zfp_stream_set_rate(zfp[1], chroma_rate, zfp_type_int32, 1, 0);
     zfp_stream_set_rate(zfp[2], chroma_rate, zfp_type_int32, 1, 0);
+#else
+    double chroma_rate = floor(8 * rate / 3 + 0.5) / 16;
+    double luma_rate = rate - 2 * chroma_rate;
+    zfp_stream_set_rate(zfp[0], luma_rate, zfp_type_int32, 2, 0);
+    zfp_stream_set_rate(zfp[1], chroma_rate, zfp_type_int32, 2, 0);
+    zfp_stream_set_rate(zfp[2], chroma_rate, zfp_type_int32, 2, 0);
 #endif
   }
 
@@ -270,15 +323,14 @@ int main(int argc, char* argv[])
       /* chroma subsample the Co and Cg bands */
       for (k = 1; k < 3; k++)
         chroma_downsample(ycocg[k]);
-#ifdef CHROMA2D
       /* compress the Y, Co, and Cg blocks */
-      for (k = 0; k < 3; k++)
-        zfp_encode_block_int32_2(zfp[k], ycocg[k]);
-#else
-      /* compress the Y, Co, and Cg blocks */
+#if PPM_CHROMA == 1
       zfp_encode_block_int32_2(zfp[0], ycocg[0]);
       zfp_encode_block_int32_1(zfp[1], ycocg[1]);
       zfp_encode_block_int32_1(zfp[2], ycocg[2]);
+#else
+      for (k = 0; k < 3; k++)
+        zfp_encode_block_int32_2(zfp[k], ycocg[k]);
 #endif
     }
 
@@ -294,19 +346,18 @@ int main(int argc, char* argv[])
       int32 rgb[3][16];
       int32 ycocg[3][16];
       uint i, j, k;
-#ifdef CHROMA2D
       /* decompress the Y, Co, and Cg blocks */
-      for (k = 0; k < 3; k++)
-        zfp_decode_block_int32_2(zfp[k], ycocg[k]);
-#else
-      /* decompress the Y, Co, and Cg blocks */
+#if PPM_CHROMA == 1
       zfp_decode_block_int32_2(zfp[0], ycocg[0]);
       zfp_decode_block_int32_1(zfp[1], ycocg[1]);
       zfp_decode_block_int32_1(zfp[2], ycocg[2]);
+#else
+      for (k = 0; k < 3; k++)
+        zfp_decode_block_int32_2(zfp[k], ycocg[k]);
+#endif
       /* reconstruct Co and Cg chroma bands */
       for (k = 1; k < 3; k++)
         chroma_upsample(ycocg[k]);
-#endif
       /* perform color space transform */
       ycocg2rgb(rgb, ycocg);
       /* demote to 8-bit integers */
@@ -329,7 +380,10 @@ int main(int argc, char* argv[])
   printf("P6\n");
   printf("%u %u\n", nx, ny);
   printf("255\n");
-  fwrite(image, sizeof(*image), 3 * nx * ny, stdout);
+  if (fwrite(image, sizeof(*image), 3 * nx * ny, stdout) != 3 * nx * ny) {
+    fprintf(stderr, "error writing image\n");
+    return EXIT_FAILURE;
+  }
   free(image);
 
   return 0;
