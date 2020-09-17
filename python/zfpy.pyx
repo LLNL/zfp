14131c302d74f2b0ad1ca05143f46b61b0f1743f
halehawk <haiyingx@ucar.edu>
2020-09-17 16:32:13 -0600

    add const to char*

--- python/zfpy.pyx
+++ python/zfpy.pyx
@@ -261,7 +261,7 @@ cpdef np.ndarray _decompress(
         raise ValueError("Cannot decompress in-place")
     _validate_4d_list(shape, "shape")
 
-    cdef char* comp_data_pointer = <char*>&compressed_data[0]
+    cdef const char* comp_data_pointer = <const char*>&compressed_data[0]
     cdef zfp_field* field = zfp_field_alloc()
     cdef bitstream* bstream = stream_open(
         comp_data_pointer,
@@ -335,7 +335,7 @@ cpdef np.ndarray decompress_numpy(
     if compressed_data is None:
         raise TypeError("compressed_data cannot be None")
 
-    cdef char* comp_data_pointer = <char *>&compressed_data[0]
+    cdef const char* comp_data_pointer = <const char *>&compressed_data[0]
     cdef zfp_field* field = zfp_field_alloc()
     cdef bitstream* bstream = stream_open(
         comp_data_pointer,
