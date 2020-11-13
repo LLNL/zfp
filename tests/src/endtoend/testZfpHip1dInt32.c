55ece61a2f42fbd8f55118ca1763e68abfbf54f2
Jieyang Chen <chenj3@ornl.gov>
2020-11-12 21:44:01 -0500

    porting device side tests

---
+++ tests/src/endtoend/testZfpHip1dInt32.c
@@ -0,0 +1,13 @@
+#include "src/encode1i.c"
+
+#include "constants/1dInt32.h"
+#include "hipExecBase.c"
+
+int main()
+{
+  const struct CMUnitTest tests[] = {
+    #include "testcases/hip.c"
+  };
+
+  return cmocka_run_group_tests(tests, setupRandomData, teardownRandomData);
+}
