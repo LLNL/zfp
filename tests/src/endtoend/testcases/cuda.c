82cc2570f45ba25c2a028ff5f7e344f8d4158bfc
lindstro <pl@llnl.gov>
2021-09-08 10:26:48 -0700

    Re-enable variable-rate CUDA tests

--- tests/src/endtoend/testcases/cuda.c
+++ tests/src/endtoend/testcases/cuda.c
@@ -6,26 +6,26 @@ _cmocka_unit_test(when_seededRandomSmoothDataGenerated_expect_ChecksumMatches),
 _cmocka_unit_test_setup_teardown(_catFunc3(given_Cuda_, DIM_INT_STR, Array_when_ZfpCompressDecompressFixedRate_expect_BitstreamAndArrayChecksumsMatch), setupDefaultStride, teardown),
 
 /* fixed-precision compression */
-// _cmocka_unit_test_setup_teardown(_catFunc3(given_Cuda_, DIM_INT_STR, Array_when_ZfpCompressFixedPrecision_expect_BitstreamAndArrayChecksumsMatch), setupDefaultStride, teardown),
+_cmocka_unit_test_setup_teardown(_catFunc3(given_Cuda_, DIM_INT_STR, Array_when_ZfpCompressFixedPrecision_expect_BitstreamAndArrayChecksumsMatch), setupDefaultStride, teardown),
 
 /* fixed-accuracy compression */
 #ifdef FL_PT_DATA
-// _cmocka_unit_test_setup_teardown(_catFunc3(given_Cuda_, DIM_INT_STR, Array_when_ZfpCompressFixedAccuracy_expect_BitstreamAndArrayChecksumsMatch), setupDefaultStride, teardown),
+_cmocka_unit_test_setup_teardown(_catFunc3(given_Cuda_, DIM_INT_STR, Array_when_ZfpCompressFixedAccuracy_expect_BitstreamAndArrayChecksumsMatch), setupDefaultStride, teardown),
 #endif
 
 /* reversed layout */
 _cmocka_unit_test_setup_teardown(_catFunc3(given_Cuda_, DIM_INT_STR, ReversedArray_when_ZfpCompressDecompressFixedRate_expect_BitstreamAndArrayChecksumsMatch), setupReversed, teardown),
-// _cmocka_unit_test_setup_teardown(_catFunc3(given_Cuda_, DIM_INT_STR, ReversedArray_when_ZfpCompressFixedPrecision_expect_BitstreamAndArrayChecksumsMatch), setupReversed, teardown),
+_cmocka_unit_test_setup_teardown(_catFunc3(given_Cuda_, DIM_INT_STR, ReversedArray_when_ZfpCompressFixedPrecision_expect_BitstreamAndArrayChecksumsMatch), setupReversed, teardown),
 #ifdef FL_PT_DATA
-// _cmocka_unit_test_setup_teardown(_catFunc3(given_Cuda_, DIM_INT_STR, ReversedArray_when_ZfpCompressFixedAccuracy_expect_BitstreamAndArrayChecksumsMatch), setupReversed, teardown),
+_cmocka_unit_test_setup_teardown(_catFunc3(given_Cuda_, DIM_INT_STR, ReversedArray_when_ZfpCompressFixedAccuracy_expect_BitstreamAndArrayChecksumsMatch), setupReversed, teardown),
 #endif
 
 #if DIMS >= 2
 /* permuted layout */
 _cmocka_unit_test_setup_teardown(_catFunc3(given_Cuda_, DIM_INT_STR, PermutedArray_when_ZfpCompressDecompressFixedRate_expect_BitstreamAndArrayChecksumsMatch), setupPermuted, teardown),
-// _cmocka_unit_test_setup_teardown(_catFunc3(given_Cuda_, DIM_INT_STR, PermutedArray_when_ZfpCompressFixedPrecision_expect_BitstreamAndArrayChecksumsMatch), setupPermuted, teardown),
+ _cmocka_unit_test_setup_teardown(_catFunc3(given_Cuda_, DIM_INT_STR, PermutedArray_when_ZfpCompressFixedPrecision_expect_BitstreamAndArrayChecksumsMatch), setupPermuted, teardown),
 #ifdef FL_PT_DATA
-// _cmocka_unit_test_setup_teardown(_catFunc3(given_Cuda_, DIM_INT_STR, PermutedArray_when_ZfpCompressFixedAccuracy_expect_BitstreamAndArrayChecksumsMatch), setupPermuted, teardown),
+ _cmocka_unit_test_setup_teardown(_catFunc3(given_Cuda_, DIM_INT_STR, PermutedArray_when_ZfpCompressFixedAccuracy_expect_BitstreamAndArrayChecksumsMatch), setupPermuted, teardown),
 #endif
 #endif
 
