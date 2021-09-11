bb4db1d238873ce506fb9c4958debeab4696cbbf
Garrett Morrison <morrison32@llnl.gov>
2021-09-10 18:44:59 -0700

    Fixed bug with cfp view ctor

--- cfp/src/template/cfpview.cpp
+++ cfp/src/template/cfpview.cpp
@@ -2,6 +2,6 @@ static CFP_CONTAINER_TYPE
 _t1(CFP_CONTAINER_TYPE, ctor)(const CFP_ARRAY_TYPE a)
 {
   CFP_CONTAINER_TYPE v;
-  v.object = new ZFP_CONTAINER_TYPE(*static_cast<const ZFP_CONTAINER_TYPE *>(a.object));
+  v.object = new ZFP_CONTAINER_TYPE(static_cast<ZFP_ARRAY_TYPE *>(a.object));
   return v;
 }
