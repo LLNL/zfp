c86be611d0981442659938044ca05cad5171d56b
gmorriso <morrison32@llnl.gov>
2022-07-28 15:09:06 -0700

    Update zfpy version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import numpy as np
 
 setup(
     name="zfpy",
-    version="0.5.5",
+    version="1.0.0",
     author="Peter Lindstrom",
     author_email="zfp@llnl.gov",
     url="https://computing.llnl.gov/projects/floating-point-compression",
