c9ec980f7f9fe261dbb2c398f873a3caabf85982
lindstro <pl@llnl.gov>
2022-07-29 17:22:16 -0700

    Update zfp homepage URL

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version="1.0.0",
     author="Peter Lindstrom",
     author_email="zfp@llnl.gov",
-    url="https://computing.llnl.gov/projects/floating-point-compression",
+    url="https://zfp.llnl.gov",
     description="zfp compression in Python",
     long_description="zfp is a compressed format for representing multidimensional floating-point and integer arrays. zfp provides compressed-array classes that support high throughput read and write random access to individual array elements. zfp also supports serial and parallel compression of whole arrays using both lossless and lossy compression with error tolerances. zfp is primarily written in C and C++ but also includes Python and Fortran bindings.",
     ext_modules=[Extension("zfpy", ["build/python/zfpy.c"],
