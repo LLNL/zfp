a4c75d49d50e3f913367e64062f5ab97667b3721
gmorriso <morrison32@llnl.gov>
2023-04-25 10:38:56 -0700

    Clean up cmake files

--- cmake/zfpy.cmake
+++ cmake/zfpy.cmake
@@ -2,10 +2,11 @@ function(zfpy_add_module name)
     # Cythonize
     add_cython_target(${name} ${zfpy_SOURCE_DIR}/${name}.pyx CXX PY3)
     add_library(${name} MODULE ${${name}})
-    target_include_directories(${name} PRIVATE ${ZFP_SOURCE_DIR}/include)
+    target_include_directories(${name} PRIVATE ${ZFP_SOURCE_DIR}/include ${NumPy_INCLUDE_DIR})
     target_link_libraries(${name} zfp)
     set_property(TARGET ${name} APPEND PROPERTY BUILD_RPATH "$<TARGET_FILE_DIR:zfp>")
     python_extension_module(${name})
+    set_target_properties(${name} PROPERTIES LIBRARY_OUTPUT_DIRECTORY ${PYLIB_BUILD_DIR})
 
     # Install to the typical python module directory
     set(python_install_lib_dir "lib/python${PYTHON_VERSION_MAJOR}.${PYTHON_VERSION_MINOR}/site-packages/")
