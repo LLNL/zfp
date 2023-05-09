fa36b248f7fe6424c293dc87782213aef496555d
gmorriso <morrison32@llnl.gov>
2023-05-09 12:34:28 -0700

    Fix test_utils include paths

--- cmake/zfpy.cmake
+++ cmake/zfpy.cmake
@@ -14,15 +14,16 @@ function(zfpy_add_module name type)
     install(TARGETS ${name} LIBRARY DESTINATION ${python_install_lib_dir})
 endfunction()
 
-
 function(zfpy_add_test_module name src_dir)
+    include_directories(${ZFP_SOURCE_DIR}/include)
+    include_directories(${ZFP_SOURCE_DIR}/python)
+    include_directories(${NumPy_INCLUDE_DIR})
+    include_directories(${ZFP_SOURCE_DIR}/tests/python)
+    include_directories(${ZFP_SOURCE_DIR}/tests/utils)
+    include_directories(${ZFP_SOURCE_DIR})
+
     add_cython_target(${name} ${src_dir}/${name}.pyx C PY3)
     add_library(${name} MODULE ${${name}})
-    target_include_directories(${name} PRIVATE 
-        ${NumPy_INCLUDE_DIR}
-        ${ZFP_SOURCE_DIR}/include
-        ${ZFP_SOURCE_DIR}/tests/utils
-    )
     target_link_libraries(${name} zfpChecksumsLib zfp genSmoothRandNumsLib stridedOperationsLib zfpCompressionParamsLib zfpHashLib)
     set_property(TARGET ${name} APPEND PROPERTY BUILD_RPATH "$<TARGET_FILE_DIR:zfp>")
     set_target_properties(${name} PROPERTIES LIBRARY_OUTPUT_DIRECTORY ${PYLIB_BUILD_DIR})
