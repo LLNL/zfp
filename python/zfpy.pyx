37004a11e24a383d82b762dc44e542ddbd93ce6c
William Silversmith <william.silversmith@gmail.com>
2024-07-03 15:26:38 -0400

    fix: setup.py + set language level for cython

--- python/zfpy.pyx
+++ python/zfpy.pyx
@@ -1,3 +1,5 @@
+# cython: language_level=3
+
 import sys
 import operator
 import functools
--- setup.py
+++ setup.py
@@ -1,15 +1,31 @@
 from setuptools import setup, Extension
-import numpy as np
+
+class NumpyImport:
+  def __repr__(self):
+    import numpy as np
+
+    return np.get_include()
+
+  __fspath__ = __repr__
 
 setup(
     name="zfpy",
+    setup_requires=["numpy", "cython"],
     version="1.0.1",
     author="Peter Lindstrom, Danielle Asher",
     author_email="zfp@llnl.gov",
     url="https://zfp.llnl.gov",
     description="zfp compression in Python",
     long_description="zfp is a compressed format for representing multidimensional floating-point and integer arrays. zfp provides compressed-array classes that support high throughput read and write random access to individual array elements. zfp also supports serial and parallel compression of whole arrays using both lossless and lossy compression with error tolerances. zfp is primarily written in C and C++ but also includes Python and Fortran bindings.",
-    ext_modules=[Extension("zfpy", ["build/python/zfpy.c"],
-                           include_dirs=["include", np.get_include()],
-                           libraries=["zfp"], library_dirs=["build/lib64", "build/lib/Release"]), language_level = "3"]
+    ext_modules=[
+        Extension(
+            "zfpy", 
+            sources=["python/zfpy.pyx"],
+            include_dirs=["include", str(NumpyImport())],
+            libraries=["zfp"], 
+            library_dirs=["build/lib64", "build/lib/Release"],
+            language_level=3,
+            lanugage="c",
+        ),
+    ],
 )
