2bb955b66bfe6737a6ee777c89d6f889ef596fd8
lindstro <pl@llnl.gov>
2025-07-02 11:09:35 -0700

    Silence warnings due to unused templates

--- src/template/utils.c
+++ src/template/utils.c
@@ -16,14 +16,14 @@
 }
 
 /* template instantiations */
-static uint _tdef1(count_up, uint, (uint size, uint unit))
-static size_t _tdef1(count_up, size_t, (size_t size, size_t unit))
-static uint64 _tdef1(count_up, uint64, (uint64 size, uint64 unit))
-static bitstream_size _tdef1(count_up, bitstream_size, (bitstream_size size, bitstream_size unit))
+/* static uint _tdef1(count_up, uint, (uint size, uint unit)) */
+/* static size_t _tdef1(count_up, size_t, (size_t size, size_t unit)) */
+/* static uint64 _tdef1(count_up, uint64, (uint64 size, uint64 unit)) */
+/* static bitstream_size _tdef1(count_up, bitstream_size, (bitstream_size size, bitstream_size unit)) */
 
 static uint _tdef1(round_up, uint, (uint size, uint unit))
-static size_t _tdef1(round_up, size_t, (size_t size, size_t unit))
-static uint64 _tdef1(round_up, uint64, (uint64 size, uint64 unit))
+/* static size_t _tdef1(round_up, size_t, (size_t size, size_t unit)) */
+/* static uint64 _tdef1(round_up, uint64, (uint64 size, uint64 unit)) */
 static bitstream_size _tdef1(round_up, bitstream_size, (bitstream_size size, bitstream_size unit))
 
 #endif
